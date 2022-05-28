de92b154b3543facc796558050e46a672fa08a9a
Marian Stanciulica <marian.stanciulica@Marians-MacBook-Pro.local>
2022-05-28 09:31:34 +0300

    Project Added

---
+++ Foodybite/Foodybite/FoodybiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodybiteApp.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodybiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
