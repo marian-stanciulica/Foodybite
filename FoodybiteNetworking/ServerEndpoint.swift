9201b0249c66fa9711bb3a079274409240f18ed9
Marian25 <marian.stanciulica@gmail.com>
2022-10-13 18:36:37 +0300

    ServerEndpoint Moved to Production

---
+++ FoodybiteNetworking/ServerEndpoint.swift
@@ -0,0 +1,49 @@
+//
+//  ServerEndpoint.swift
+//  FoodybiteNetworking
+//
+//  Created by Marian Stanciulica on 13.10.2022.
+//
+
+enum ServerEndpoint {
+    case signup(name: String, email: String, password: String, confirmPassword: String)
+    case login(email: String, password: String)
+    
+    var path: String {
+        switch self {
+        case .signup:
+            return "/auth/signup"
+        case .login:
+            return "/auth/login"
+        }
+    }
+    
+    var method: String {
+        "POST"
+    }
+    
+    var headers: [String : String] {
+        ["Content-Type" : "application/json"]
+    }
+    
+    var body: [String : String] {
+        var body = [String : String]()
+        
+        switch self {
+        case let .signup(name, email, password, confirmPassword):
+            body["name"] = name
+            body["email"] = email
+            body["password"] = password
+            body["confirm_password"] = confirmPassword
+        case let .login(email, password):
+            body["email"] = email
+            body["password"] = password
+        }
+        
+        return body
+    }
+    
+    var urlParams: [String : String] {
+        [:]
+    }
+}
--- FoodybiteNetworkingTests/ServerEndpointTests.swift
+++ FoodybiteNetworkingTests/ServerEndpointTests.swift
@@ -6,50 +6,7 @@
 //
 
 import XCTest
-
-enum ServerEndpoint {
-    case signup(name: String, email: String, password: String, confirmPassword: String)
-    case login(email: String, password: String)
-    
-    var path: String {
-        switch self {
-        case .signup:
-            return "/auth/signup"
-        case .login:
-            return "/auth/login"
-        }
-        
-    }
-    
-    var method: String {
-        "POST"
-    }
-    
-    var headers: [String : String] {
-        ["Content-Type" : "application/json"]
-    }
-    
-    var body: [String : String] {
-        var body = [String : String]()
-        
-        switch self {
-        case let .signup(name, email, password, confirmPassword):
-            body["name"] = name
-            body["email"] = email
-            body["password"] = password
-            body["confirm_password"] = confirmPassword
-        case let .login(email, password):
-            body["email"] = email
-            body["password"] = password
-        }
-        
-        return body
-    }
-    
-    var urlParams: [String : String] {
-        [:]
-    }
-}
+@testable import FoodybiteNetworking
 
 final class ServerEndpointTests: XCTestCase {
 
