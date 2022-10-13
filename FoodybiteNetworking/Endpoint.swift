c3a0eb659d0103a24d48f1898b4bcc1909cfeebf
Marian25 <marian.stanciulica@gmail.com>
2022-10-13 16:18:55 +0300

    Endpoint & RequestType Added

--- FoodybiteNetworking/Endpoint.swift
+++ FoodybiteNetworking/Endpoint.swift
@@ -5,6 +5,12 @@
 //  Created by Marian Stanciulica on 13.10.2022.
 //
 
+import Foundation
+
 protocol Endpoint {
-    
+    var path: String { get }
+    var method: RequestType { get }
+    var headers: [String: String] { get }
+    var body: [String: String] { get }
+    var urlParams: [String: String] { get }
 }
---
+++ FoodybiteNetworking/RequestType.swift
@@ -0,0 +1,14 @@
+//
+//  RequestType.swift
+//  FoodybiteNetworking
+//
+//  Created by Marian Stanciulica on 13.10.2022.
+//
+
+enum RequestType: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
