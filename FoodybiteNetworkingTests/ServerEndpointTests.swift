5809ef05e80e2eb4cb245238d0d287aa0e1fe419
Marian25 <marian.stanciulica@gmail.com>
2022-10-13 17:13:26 +0300

    ServerEndpointTests Added

---
+++ FoodybiteNetworkingTests/ServerEndpointTests.swift
@@ -0,0 +1,14 @@
+//
+//  ServerEndpointTests.swift
+//  FoodybiteNetworkingTests
+//
+//  Created by Marian Stanciulica on 13.10.2022.
+//
+
+import XCTest
+
+final class ServerEndpointTests: XCTestCase {
+
+    
+
+}
