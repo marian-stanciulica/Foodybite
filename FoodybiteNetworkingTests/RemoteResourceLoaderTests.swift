d9a101cb70686d8df87cc72d01d49f8851b00f73
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 10:30:47 +0300

    RemoteResourceLoader: throws error for response with non 2xx status code

--- FoodybiteNetworkingTests/RemoteResourceLoaderTests.swift
+++ FoodybiteNetworkingTests/RemoteResourceLoaderTests.swift
@@ -11,24 +11,55 @@ import XCTest
 class RemoteResourceLoader {
     private let client: HTTPClientSpy
     
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
     init(client: HTTPClientSpy) {
         self.client = client
     }
     
     func get(for urlRequest: URLRequest) throws {
-        try client.get(for: urlRequest)
+        let response: HTTPURLResponse
+        
+        do {
+            (_, response) = try client.get(for: urlRequest)
+        } catch {
+            throw Error.connectivity
+        }
+        
+        guard (200..<300).contains(response.statusCode) else {
+            throw Error.invalidData
+        }
     }
 }
 
 class HTTPClientSpy {
     var urlRequests = [URLRequest]()
     var errorToThrow: NSError?
+    var successResult: (data: Data, response: HTTPURLResponse)?
     
-    func get(for urlRequest: URLRequest) throws {
+    private let defaultResponse = (
+        "any data".data(using: .utf8)!,
+        HTTPURLResponse(url: URL(string: "http://any-url.com")!,
+                        statusCode: 200,
+                        httpVersion: nil,
+                        headerFields: nil)!
+    )
+    
+    func get(for urlRequest: URLRequest) throws -> (Data, HTTPURLResponse) {
+        urlRequests.append(urlRequest)
+        
+        if let successResult = successResult {
+            return successResult
+        }
+        
         if let error = errorToThrow {
             throw error
         }
-        urlRequests.append(urlRequest)
+        
+        return defaultResponse
     }
 }
 
@@ -62,9 +93,8 @@ final class RemoteResourceLoaderTests: XCTestCase {
     func test_get_throwErrorOnClientError() throws {
         let (sut, client) = makeSUT()
         let urlRequest = try EndpointStub.stub.createURLRequest()
-        let expectedError = NSError(domain: "any error", code: 1)
         
-        client.errorToThrow = expectedError
+        client.errorToThrow = NSError(domain: "any error", code: 1)
         
         var receivedError: NSError?
         do {
@@ -73,9 +103,27 @@ final class RemoteResourceLoaderTests: XCTestCase {
             receivedError = error as NSError
         }
         
-        XCTAssertEqual(receivedError?.domain, expectedError.domain)
-        XCTAssertEqual(receivedError?.code, expectedError.code)
+        XCTAssertEqual(receivedError, RemoteResourceLoader.Error.connectivity as NSError)
     }
+    
+    func test_get_throwErrorOnNon2xxStatusCodeResponse() throws {
+        let (sut, client) = makeSUT()
+        let urlRequest = try EndpointStub.stub.createURLRequest()
+        
+        let anyData = "any data".data(using: .utf8)!
+        let response = HTTPURLResponse(url: urlRequest.url!, statusCode: 300, httpVersion: nil, headerFields: nil)!
+        client.successResult = (data: anyData, response: response)
+        
+        var receivedError: NSError?
+        do {
+            try sut.get(for: urlRequest)
+        } catch {
+            receivedError = error as NSError
+        }
+        
+        XCTAssertEqual(receivedError, RemoteResourceLoader.Error.invalidData as NSError)
+    }
+    
     
     // MARK: - Helpers
     
