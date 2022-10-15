2d5d860439045a043673899f937b5f7685214e9f
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 18:51:02 +0300

    APIService: sends request on login method

--- FoodybiteNetworkingTests/API Service/APIServiceTests.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests.swift
@@ -6,9 +6,45 @@
 //
 
 import XCTest
+@testable import FoodybiteNetworking
+
+struct LoginResponse: Decodable {
+    
+}
+
+class APIService {
+    private let loader: ResourceLoader
+    
+    init(loader: ResourceLoader) {
+        self.loader = loader
+    }
+    
+    func login() async throws -> LoginResponse {
+        return try await loader.get(for: URLRequest(url: URL(string: "http://any-url.com")!))
+    }
+}
+
+class ResourceLoaderSpy: ResourceLoader {
+    var requestsCount = 0
+    
+    
+    func get<T>(for urlRequest: URLRequest) async throws -> T where T : Decodable {
+        requestsCount += 1
+        return LoginResponse() as! T
+    }
+    
+}
 
 final class APIServiceTests: XCTestCase {
 
+    func test_login_sendsURLRequstToRemoteLoader() async throws {
+        let loader = ResourceLoaderSpy()
+        let sut = APIService(loader: loader)
+        
+        _ = try await sut.login()
+        
+        XCTAssertEqual(loader.requestsCount, 1)
+    }
     
 
 }
