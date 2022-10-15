2e7e292284f6b0502cfb4f1c8b5b3701d83a744f
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 19:19:07 +0300

    APIService: test added for conformance check to LoginService

--- FoodybiteNetworkingTests/API Service/APIServiceTests.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests.swift
@@ -9,10 +9,15 @@ import XCTest
 @testable import FoodybiteNetworking
 
 struct LoginResponse: Decodable {
-    
+    let name: String
+    let email: String
 }
 
-class APIService {
+protocol LoginService {
+    func login(email: String, password: String) async throws -> LoginResponse
+}
+
+class APIService: LoginService {
     private let loader: ResourceLoader
     
     init(loader: ResourceLoader) {
@@ -27,16 +32,23 @@ class APIService {
 }
 
 class ResourceLoaderSpy: ResourceLoader {
+    var response = LoginResponse(name: "name", email: "test@email.com")
     var requests = [URLRequest]()
     
     func get<T>(for urlRequest: URLRequest) async throws -> T where T : Decodable {
         requests.append(urlRequest)
-        return LoginResponse() as! T
+        return response as! T
     }
     
 }
 
 final class APIServiceTests: XCTestCase {
+    
+    func test_conformsToLoginService() {
+        let loader = ResourceLoaderSpy()
+        let sut = APIService(loader: loader)
+        XCTAssertNotNil(sut as LoginService)
+    }
     
     func test_login_useLoginEndpointToCreateURLRequest() async throws {
         let loginEndpoint = ServerEndpoint.login(email: "test@test.com", password: "123@password$321")
@@ -48,6 +60,5 @@ final class APIServiceTests: XCTestCase {
         let urlRequest = try loginEndpoint.createURLRequest()
         XCTAssertEqual(loader.requests, [urlRequest])
     }
-    
 
 }
