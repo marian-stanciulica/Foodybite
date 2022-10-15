6106fd8c6ddc624598cce391f0c19ca8edf23c6f
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 19:00:12 +0300

    APIService: use login endpoint to create urlRequest

--- FoodybiteNetworkingTests/API Service/APIServiceTests.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests.swift
@@ -19,31 +19,34 @@ class APIService {
         self.loader = loader
     }
     
-    func login() async throws -> LoginResponse {
-        return try await loader.get(for: URLRequest(url: URL(string: "http://any-url.com")!))
+    func login(email: String, password: String) async throws -> LoginResponse {
+        let endpoint = ServerEndpoint.login(email: email, password: password)
+        let urlRequest = try endpoint.createURLRequest()
+        return try await loader.get(for: urlRequest)
     }
 }
 
 class ResourceLoaderSpy: ResourceLoader {
-    var requestsCount = 0
-    
+    var requests = [URLRequest]()
     
     func get<T>(for urlRequest: URLRequest) async throws -> T where T : Decodable {
-        requestsCount += 1
+        requests.append(urlRequest)
         return LoginResponse() as! T
     }
     
 }
 
 final class APIServiceTests: XCTestCase {
-
-    func test_login_sendsURLRequstToRemoteLoader() async throws {
+    
+    func test_login_useLoginEndpointToCreateURLRequest() async throws {
+        let loginEndpoint = ServerEndpoint.login(email: "test@test.com", password: "123@password$321")
         let loader = ResourceLoaderSpy()
         let sut = APIService(loader: loader)
         
-        _ = try await sut.login()
+        _ = try await sut.login(email: "test@test.com", password: "123@password$321")
         
-        XCTAssertEqual(loader.requestsCount, 1)
+        let urlRequest = try loginEndpoint.createURLRequest()
+        XCTAssertEqual(loader.requests, [urlRequest])
     }
     
 
