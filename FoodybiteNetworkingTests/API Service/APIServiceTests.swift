3a1097971c193cf5f4b1d0ec752b9aa16388724f
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 19:30:06 +0300

    APIService: received expected login response

--- FoodybiteNetworkingTests/API Service/APIServiceTests.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests.swift
@@ -32,17 +32,22 @@ class APIService: LoginService {
 }
 
 class ResourceLoaderSpy: ResourceLoader {
-    var response = LoginResponse(name: "name", email: "test@email.com")
+    private let response: LoginResponse
     var requests = [URLRequest]()
+    
+    init(response: LoginResponse = LoginResponse(name: "any name", email: "any@email.com")) {
+        self.response = response
+    }
     
     func get<T>(for urlRequest: URLRequest) async throws -> T where T : Decodable {
         requests.append(urlRequest)
         return response as! T
     }
-    
 }
 
 final class APIServiceTests: XCTestCase {
+    
+    // MARK: - LoginService Tests
     
     func test_conformsToLoginService() {
         let loader = ResourceLoaderSpy()
@@ -51,14 +56,41 @@ final class APIServiceTests: XCTestCase {
     }
     
     func test_login_useLoginEndpointToCreateURLRequest() async throws {
-        let loginEndpoint = ServerEndpoint.login(email: "test@test.com", password: "123@password$321")
+        let email = anyEmail()
+        let password = anyPassword()
+        
+        let loginEndpoint = ServerEndpoint.login(email: email, password: password)
         let loader = ResourceLoaderSpy()
         let sut = APIService(loader: loader)
         
-        _ = try await sut.login(email: "test@test.com", password: "123@password$321")
+        _ = try await sut.login(email: email, password: password)
         
         let urlRequest = try loginEndpoint.createURLRequest()
         XCTAssertEqual(loader.requests, [urlRequest])
+    }
+    
+    func test_login_receiveExpectedLoginResponse() async throws {
+        let email = anyEmail()
+        let password = anyPassword()
+        let expectedResponse = LoginResponse(name: "some name", email: "some@email.com")
+        
+        let loader = ResourceLoaderSpy(response: expectedResponse)
+        let sut = APIService(loader: loader)
+        
+        let receivedResponse = try await sut.login(email: email, password: password)
+        
+        XCTAssertEqual(expectedResponse.name, receivedResponse.name)
+        XCTAssertEqual(expectedResponse.email, receivedResponse.email)
+    }
+    
+    // MARK: - Helpers
+    
+    private func anyEmail() -> String {
+        "test@test.com"
+    }
+    
+    private func anyPassword() -> String {
+        "123@password$321"
     }
 
 }
