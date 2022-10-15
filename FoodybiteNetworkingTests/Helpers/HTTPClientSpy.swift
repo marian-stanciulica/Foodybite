e292b1b0d9a0d8d96683c52c9ce7524c7e7d4a11
Marian25 <marian.stanciulica@gmail.com>
2022-10-15 13:26:53 +0300

    HTTPClientSpy moved to its own file

---
+++ FoodybiteNetworkingTests/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,44 @@
+//
+//  HTTPClientSpy.swift
+//  FoodybiteNetworkingTests
+//
+//  Created by Marian Stanciulica on 15.10.2022.
+//
+
+import Foundation
+@testable import FoodybiteNetworking
+
+class HTTPClientSpy: HTTPClient {
+    var urlRequests = [URLRequest]()
+    var result: Result<(Data, HTTPURLResponse), NSError>?
+    
+    func get(for urlRequest: URLRequest) throws -> (Data, HTTPURLResponse) {
+        urlRequests.append(urlRequest)
+        
+        if let result = result {
+            switch result {
+            case let .failure(error):
+                throw error
+            case let .success(result):
+                return result
+            }
+        }
+        
+        return (anyLoginMocksData(), anyHttpUrlResponse())
+    }
+    
+    private func anyHttpUrlResponse() -> HTTPURLResponse {
+        HTTPURLResponse(url: URL(string: "http://any-url.com")!,
+                        statusCode: 200,
+                        httpVersion: nil,
+                        headerFields: nil)!
+    }
+    
+    private func anyLoginMocksData() -> Data {
+        let loginMocks = [
+            CodableMock(name: "name 1", password: "password 1"),
+            CodableMock(name: "name 2", password: "password 2")
+        ]
+        return try! JSONEncoder().encode(loginMocks)
+    }
+}
--- FoodybiteNetworkingTests/RemoteResourceLoaderTests.swift
+++ FoodybiteNetworkingTests/RemoteResourceLoaderTests.swift
@@ -8,41 +8,6 @@
 import XCTest
 @testable import FoodybiteNetworking
 
-class HTTPClientSpy: HTTPClient {
-    var urlRequests = [URLRequest]()
-    var result: Result<(Data, HTTPURLResponse), NSError>?
-    
-    func get(for urlRequest: URLRequest) throws -> (Data, HTTPURLResponse) {
-        urlRequests.append(urlRequest)
-        
-        if let result = result {
-            switch result {
-            case let .failure(error):
-                throw error
-            case let .success(result):
-                return result
-            }
-        }
-        
-        return (anyLoginMocksData(), anyHttpUrlResponse())
-    }
-    
-    private func anyHttpUrlResponse() -> HTTPURLResponse {
-        HTTPURLResponse(url: URL(string: "http://any-url.com")!,
-                        statusCode: 200,
-                        httpVersion: nil,
-                        headerFields: nil)!
-    }
-    
-    private func anyLoginMocksData() -> Data {
-        let loginMocks = [
-            CodableMock(name: "name 1", password: "password 1"),
-            CodableMock(name: "name 2", password: "password 2")
-        ]
-        return try! JSONEncoder().encode(loginMocks)
-    }
-}
-
 final class RemoteResourceLoaderTests: XCTestCase {
 
     func test_init_noRequestTriggered() {
