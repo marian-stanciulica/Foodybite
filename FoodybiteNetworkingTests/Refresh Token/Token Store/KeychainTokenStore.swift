709c4067484d46e32eb25791291b3ed0770510f2
Marian25 <marian.stanciulica@gmail.com>
2022-10-16 20:15:10 +0300

    KeychainTokenStore: read throws error when store is empty

--- FoodybiteNetworkingTests/Refresh Token/Token Store/KeychainTokenStore.swift
+++ FoodybiteNetworkingTests/Refresh Token/Token Store/KeychainTokenStore.swift
@@ -7,8 +7,35 @@
 
 import XCTest
 
-final class KeychainTokenStore: XCTestCase {
+struct AuthToken: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
 
+class KeychainTokenStore {
+    
+    private enum Error: Swift.Error {
+        case notFound
+    }
+    
+    func read() throws {
+        throw Error.notFound
+    }
+    
+}
+
+final class KeychainTokenStoreTests: XCTestCase {
+    private var sut: KeychainTokenStore!
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut = KeychainTokenStore()
+    }
+    
+    func test_read_shouldThrowWhenNoTokenInStore() {
+        XCTAssertThrowsError(try sut.read())
+    }
     
 
 }
