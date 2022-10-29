66fad4b2b394c7693984ad6816db9c4647ff0fe5
Marian25 <marian.stanciulica@gmail.com>
2022-10-29 09:43:48 +0300

    Persistence target added

---
+++ FoodybitePersistence/FoodybitePersistence.h
@@ -0,0 +1,18 @@
+//
+//  FoodybitePersistence.h
+//  FoodybitePersistence
+//
+//  Created by Marian Stanciulica on 29.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoodybitePersistence.
+FOUNDATION_EXPORT double FoodybitePersistenceVersionNumber;
+
+//! Project version string for FoodybitePersistence.
+FOUNDATION_EXPORT const unsigned char FoodybitePersistenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoodybitePersistence/PublicHeader.h>
+
+
