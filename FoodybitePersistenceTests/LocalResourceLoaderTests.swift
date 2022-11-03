76b03d396727f1259acd4536bee86083ada02540
Marian25 <marian.stanciulica@gmail.com>
2022-11-03 14:15:28 +0200

    'ResourceStoreSpy' moved to its own file

--- FoodybitePersistenceTests/LocalResourceLoaderTests.swift
+++ FoodybitePersistenceTests/LocalResourceLoaderTests.swift
@@ -8,68 +8,6 @@
 import XCTest
 import FoodybitePersistence
 
-class ResourceStoreSpy<T>: ResourceStore {
-    enum Message {
-        case read
-        case write
-        case delete
-    }
-    
-    struct CompletionNotSet: Error {}
-    
-    private(set) var messages = [Message]()
-    
-    private(set) var readResult: Result<T, Error>?
-    
-    private(set) var writeError: Error? = nil
-    private(set) var writeParameter: T?
-
-    private(set) var deleteError: Error? = nil
-    
-    func read() async throws -> T {
-        messages.append(.read)
-        
-        if let readCompletion = readResult {
-            return try readCompletion.get()
-        } else {
-            throw CompletionNotSet()
-        }
-    }
-    
-    func setRead(error: Error) {
-        readResult = .failure(error)
-    }
-    
-    func setRead(returnedObject object: T) {
-        readResult = .success(object)
-    }
-    
-    func write(_ object: T) async throws {
-        messages.append(.write)
-        writeParameter = object
-        
-        if let writeError = writeError {
-            throw writeError
-        }
-    }
-    
-    func setWrite(error: Error?) {
-        writeError = error
-    }
-    
-    func delete(_ type: T.Type) async throws {
-        messages.append(.delete)
-        
-        if let deleteError = deleteError {
-            throw deleteError
-        }
-    }
-    
-    func setDeletion(error: Error?) {
-        deleteError = error
-    }
-}
-
 final class LocalResourceLoaderTests: XCTestCase {
 
     func test_init_doesNotReceiveMessagesUponCreation() {
