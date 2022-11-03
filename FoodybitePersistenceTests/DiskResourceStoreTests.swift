fc98944366748d3726ddda9ac6e055cd2b6a21e2
Marian25 <marian.stanciulica@gmail.com>
2022-11-03 15:03:05 +0200

    DiskResourceStore delivers resource on cache hit

--- FoodybitePersistenceTests/DiskResourceStoreTests.swift
+++ FoodybitePersistenceTests/DiskResourceStoreTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class DiskResourceStore<T> {
+class DiskResourceStore<T: Codable> {
     private let storeURL: URL
     
     private struct CacheMissError: Error {}
@@ -17,13 +17,39 @@ class DiskResourceStore<T> {
     }
     
     func read() async throws -> T {
-        throw CacheMissError()
+        guard let data = try? Data(contentsOf: storeURL) else {
+            throw CacheMissError()
+        }
+        
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
     }
     
+    func write(_ object: T) async throws {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(object)
+        try data.write(to: storeURL)
+    }
+    
+    func delete(_ type: T.Type) async throws {
+        
+    }
 }
 
 final class DiskResourceStoreTests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_read_deliversErrorOnCacheMiss() async {
         let sut = makeSUT()
         
@@ -35,6 +61,16 @@ final class DiskResourceStoreTests: XCTestCase {
         
         await expectReadToFail(sut: sut)
         await expectReadToFail(sut: sut)
+    }
+    
+    func test_read_deliverResourceOnCacheHit() async throws {
+        let sut = makeSUT()
+        let expectedResource = anyResource()
+        
+        try await sut.write(expectedResource)
+        let receivedResource = try await sut.read()
+        
+        XCTAssertEqual(receivedResource, expectedResource)
     }
     
     // MARK: - Helpers
@@ -59,8 +95,24 @@ final class DiskResourceStoreTests: XCTestCase {
             .appendingPathComponent("\(type(of: self)).store")
     }
     
-    private struct TestingResource {
-        let resource = "any resource"
+    private func anyResource() -> TestingResource {
+        return TestingResource(resource: "any resource")
+    }
+    
+    private struct TestingResource: Codable, Equatable {
+        let resource: String
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
    
 }
