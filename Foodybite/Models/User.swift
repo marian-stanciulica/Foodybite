43a961c0f9039d7766b39971b9ffa917424e087f
Marian25 <marian.stanciulica@gmail.com>
2022-11-05 09:22:28 +0200

    User model added

---
+++ Foodybite/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 05.11.2022.
+//
+
+import Foundation
+
+struct User {
+    let id: UUID
+    let name: String
+    let email: String
+    let profileImage: URL
+}
