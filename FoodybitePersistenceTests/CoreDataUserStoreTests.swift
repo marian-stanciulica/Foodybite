ddf646fd5c7b041511c7fd935ab84971f962768f
Marian25 <marian.stanciulica@gmail.com>
2022-11-05 15:01:58 +0200

    CoreDataUserStoreTests file added

---
+++ FoodybitePersistenceTests/CoreDataUserStoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataUserStoreTests.swift
+//  FoodybitePersistenceTests
+//
+//  Created by Marian Stanciulica on 05.11.2022.
+//
+
+import XCTest
+
+final class CoreDataUserStoreTests: XCTestCase {
+
+    
+
+}
