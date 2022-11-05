bb310cfb63eabb5a445df24ca16911ddcaf67026
Marian25 <marian.stanciulica@gmail.com>
2022-11-05 09:30:30 +0200

    DomainModels Framework Added

---
+++ DomainModels/DomainModels.h
@@ -0,0 +1,18 @@
+//
+//  DomainModels.h
+//  DomainModels
+//
+//  Created by Marian Stanciulica on 05.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DomainModels.
+FOUNDATION_EXPORT double DomainModelsVersionNumber;
+
+//! Project version string for DomainModels.
+FOUNDATION_EXPORT const unsigned char DomainModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DomainModels/PublicHeader.h>
+
+
