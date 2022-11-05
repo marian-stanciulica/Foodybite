2c4369022db7963c8cac5feb447a9ed184a412a6
Marian25 <marian.stanciulica@gmail.com>
2022-11-05 09:40:29 +0200

    DiskResourceStore moved to production

---
+++ FoodybitePersistence/DiskResourceStore.swift
@@ -0,0 +1,39 @@
+//
+//  DiskResourceStore.swift
+//  FoodybitePersistence
+//
+//  Created by Marian Stanciulica on 05.11.2022.
+//
+
+import Foundation
+
+public class DiskResourceStore<T: Codable> {
+    private let storeURL: URL
+    
+    private struct CacheMissError: Error {}
+    
+    public init(storeURL: URL) {
+        self.storeURL = storeURL.appending(path: "\(T.self).resource")
+    }
+    
+    public func read() async throws -> T {
+        guard let data = try? Data(contentsOf: storeURL) else {
+            throw CacheMissError()
+        }
+        
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    public func write(_ object: T) async throws {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(object)
+        try data.write(to: storeURL)
+    }
+    
+    public func delete(_ type: T.Type) async throws {
+        if FileManager.default.fileExists(atPath: storeURL.path()) {
+            try FileManager.default.removeItem(at: storeURL)
+        }
+    }
+}
--- FoodybitePersistenceTests/DiskResourceStoreTests.swift
+++ FoodybitePersistenceTests/DiskResourceStoreTests.swift
@@ -6,37 +6,7 @@
 //
 
 import XCTest
-
-class DiskResourceStore<T: Codable> {
-    private let storeURL: URL
-    
-    private struct CacheMissError: Error {}
-    
-    init(storeURL: URL) {
-        self.storeURL = storeURL.appending(path: "\(T.self).resource")
-    }
-    
-    func read() async throws -> T {
-        guard let data = try? Data(contentsOf: storeURL) else {
-            throw CacheMissError()
-        }
-        
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    }
-    
-    func write(_ object: T) async throws {
-        let encoder = JSONEncoder()
-        let data = try encoder.encode(object)
-        try data.write(to: storeURL)
-    }
-    
-    func delete(_ type: T.Type) async throws {
-        if FileManager.default.fileExists(atPath: storeURL.path()) {
-            try FileManager.default.removeItem(at: storeURL)
-        }
-    }
-}
+import FoodybitePersistence
 
 final class DiskResourceStoreTests: XCTestCase {
 
