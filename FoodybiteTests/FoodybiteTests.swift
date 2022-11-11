7deab069aba6df3d82249bb5b31e15784d18d945
Marian25 <marian.stanciulica@gmail.com>
2022-11-11 09:02:46 +0200

    RegisterViewModel.register throws empty email error when email textfield is empty

--- FoodybiteTests/FoodybiteTests.swift
+++ FoodybiteTests/FoodybiteTests.swift
@@ -10,19 +10,26 @@ import Combine
 
 class RegisterViewModel {
     
+    var name = ""
+    
     enum RegistrationError: Error {
         case emptyName
+        case emptyEmail
     }
     
     func register() throws {
-        throw RegistrationError.emptyName
+        if name.isEmpty {
+            throw RegistrationError.emptyName
+        }
+        
+        throw RegistrationError.emptyEmail
     }
     
 }
 
 final class RegisterViewModelTests: XCTestCase {
 
-    func test_register_triggerNameIsEmptyErrorOnEmptyNameTextField() {
+    func test_register_triggerEmptyNameErrorOnEmptyNameTextField() {
         let sut = RegisterViewModel()
         
         do {
@@ -30,6 +37,18 @@ final class RegisterViewModelTests: XCTestCase {
             XCTFail("Register should fail with empty name")
         } catch {
             XCTAssertEqual(error as? RegisterViewModel.RegistrationError, .emptyName)
+        }
+    }
+    
+    func test_register_triggerEmptyEmailErrorOnEmptyEmailTextField() {
+        let sut = RegisterViewModel()
+        sut.name = "any name"
+        
+        do {
+            try sut.register()
+            XCTFail("Register should fail with empty email")
+        } catch {
+            XCTAssertEqual(error as? RegisterViewModel.RegistrationError, .emptyEmail)
         }
     }
 
