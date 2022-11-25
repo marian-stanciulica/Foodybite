899c843030033ac264c825515097f5206e692aa0
Marian25 <marian.stanciulica@gmail.com>
2022-11-25 18:39:48 +0200

    ProfileView NavigationView removed

--- Foodybite/Views/Profile/ProfileView.swift
+++ Foodybite/Views/Profile/ProfileView.swift
@@ -15,69 +15,67 @@ struct ProfileView: View {
     @State var editProfileAlertDisplayed = false
 
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    Image("profile_picture_test")
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                        .cornerRadius(100)
+        ScrollView {
+            VStack {
+                Image("profile_picture_test")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(100)
 
-                    Text("John Williams")
-                        .font(.title)
+                Text("John Williams")
+                    .font(.title)
 
-                    Text("john.williams@gmail.com")
-                        .font(.body)
+                Text("john.williams@gmail.com")
+                    .font(.body)
 
-                    HStack {
-                        StatsView(stats: "250", description: "Reviews")
+                HStack {
+                    StatsView(stats: "250", description: "Reviews")
 
-                        Spacer()
-                        Divider()
-                        Spacer()
+                    Spacer()
+                    Divider()
+                    Spacer()
 
-                        StatsView(stats: "100k", description: "Followers")
+                    StatsView(stats: "100k", description: "Followers")
 
-                        Spacer()
-                        Divider()
-                        Spacer()
+                    Spacer()
+                    Divider()
+                    Spacer()
 
-                        StatsView(stats: "30", description: "Following")
+                    StatsView(stats: "30", description: "Following")
+                }
+                .padding(.horizontal, 56)
+                .padding(.vertical)
+
+                HStack {
+                    MarineButton(title: "Edit Profile") {
+                        editProfileAlertDisplayed = true
                     }
-                    .padding(.horizontal, 56)
-                    .padding(.vertical)
+                    .padding(.horizontal)
 
-                    HStack {
-                        MarineButton(title: "Edit Profile") {
-                            editProfileAlertDisplayed = true
-                        }
-                        .padding(.horizontal)
-
-                        WhiteButton(title: "Settings") {
-                            goToSettings()
-                        }
-                        .padding(.horizontal)
+                    WhiteButton(title: "Settings") {
+                        goToSettings()
                     }
-                    .padding(.horizontal, 32)
+                    .padding(.horizontal)
+                }
+                .padding(.horizontal, 32)
 
-                    Rectangle()
-                        .frame(maxWidth: .infinity, maxHeight: 2)
-                        .foregroundColor(.gray.opacity(0.2))
-                        .padding(.top)
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 2)
+                    .foregroundColor(.gray.opacity(0.2))
+                    .padding(.top)
 
-                    LazyVStack {
-                        ForEach(0...50, id: \.self) { _ in
-                            RestaurantCell()
-                                .background(.white)
-                                .cornerRadius(16)
-                                .aspectRatio(1, contentMode: .fit)
-                                .overlay(
-                                     RoundedRectangle(cornerRadius: 16)
-                                        .stroke(Color.gray.opacity(0.2), lineWidth: 2)
-                                )
-                                .padding(.horizontal, 16)
-                                .padding(.vertical, 4)
-                        }
+                LazyVStack {
+                    ForEach(0...50, id: \.self) { _ in
+                        RestaurantCell()
+                            .background(.white)
+                            .cornerRadius(16)
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(
+                                 RoundedRectangle(cornerRadius: 16)
+                                    .stroke(Color.gray.opacity(0.2), lineWidth: 2)
+                            )
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 4)
                     }
                 }
             }
