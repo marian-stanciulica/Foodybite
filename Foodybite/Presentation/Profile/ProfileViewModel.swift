2bd4ae7a29c20f866abff8522c4e67fafe93f3a4
Marian25 <marian.stanciulica@gmail.com>
2022-11-25 18:18:24 +0200

    ProfileViewModel delete account throws error when AccountService throws error

---
+++ Foodybite/Presentation/Profile/ProfileViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewModel.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 25.11.2022.
+//
+
+import Foundation
+import FoodybiteNetworking
+
+public final class ProfileViewModel {
+    public enum Error: String, Swift.Error {
+        case serverError = "Invalid Credentials"
+    }
+    
+    public enum Result: Equatable {
+        case notTriggered
+        case success
+        case failure(Error)
+    }
+    
+    private let accountService: AccountService
+    @Published public var result: Result = .notTriggered
+    
+    public init(accountService: AccountService) {
+        self.accountService = accountService
+    }
+    
+    public func deleteAccount() async {
+        await updateResult(.failure(.serverError))
+    }
+    
+    @MainActor private func updateResult(_ newValue: Result) {
+        result = newValue
+    }
+}
---
+++ FoodybiteTests/Presentation/Profile/ProfileViewModelTests.swift
@@ -0,0 +1,64 @@
+//
+//  ProfileViewModelTests.swift
+//  FoodybiteTests
+//
+//  Created by Marian Stanciulica on 25.11.2022.
+//
+
+import XCTest
+import Foodybite
+import FoodybiteNetworking
+
+final class ProfileViewModelTests: XCTestCase {
+
+    func test_deleteAccount_throwsErrorWhenAccountServiceThrowsError() async {
+        let (sut, accountServiceSpy) = makeSUT()
+        
+        let expectedError = anyNSError()
+        accountServiceSpy.errorToThrow = expectedError
+        
+        await assertDeleteAccount(on: sut, withExpectedResult: .failure(.serverError))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: ProfileViewModel, accountServiceSpy: AccountServiceSpy) {
+        let accountServiceSpy = AccountServiceSpy()
+        let sut = ProfileViewModel(accountService: accountServiceSpy)
+        return (sut, accountServiceSpy)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 1)
+    }
+    
+    private func assertDeleteAccount(on sut: ProfileViewModel,
+                                     withExpectedResult expectedResult: ProfileViewModel.Result,
+                                     file: StaticString = #file,
+                                     line: UInt = #line) async {
+        let resultSpy = PublisherSpy(sut.$result.eraseToAnyPublisher())
+
+        XCTAssertEqual(resultSpy.results, [.notTriggered], file: file, line: line)
+        
+        await sut.deleteAccount()
+        
+        XCTAssertEqual(resultSpy.results, [.notTriggered, expectedResult], file: file, line: line)
+        resultSpy.cancel()
+    }
+                               
+    private class AccountServiceSpy: AccountService {
+        var errorToThrow: Error?
+        private(set) var deleteAccountCallCount = 0
+
+        func deleteAccount() async throws {
+            deleteAccountCallCount += 1
+
+            if let errorToThrow = errorToThrow {
+                throw errorToThrow
+            }
+        }
+        
+        func updateAccount(name: String, email: String, profileImage: Data?) async throws {}
+    }
+    
+}
