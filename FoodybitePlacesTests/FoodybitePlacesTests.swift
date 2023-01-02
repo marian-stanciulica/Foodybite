4cb8eceac49e3ec8f6923b5aa5410ae85c1b2cde
Marian25 <marian.stanciulica@gmail.com>
2023-01-02 10:07:00 +0200

    FoodybitePlaces framework Added

---
+++ FoodybitePlacesTests/FoodybitePlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodybitePlacesTests.swift
+//  FoodybitePlacesTests
+//
+//  Created by Marian Stanciulica on 02.01.2023.
+//
+
+import XCTest
+@testable import FoodybitePlaces
+
+final class FoodybitePlacesTests: XCTestCase {
+
+    func test() {
+        XCTFail("Test Failed!")
+    }
+
+}
