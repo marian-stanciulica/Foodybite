4b580d3f8b8ca0dd7a29ee1f2b147739e9f531ec
Marian25 <marian.stanciulica@gmail.com>
2023-01-26 15:27:27 +0200

    Sign Up request tested end to end to check the actual connection to the local server

---
+++ FoodybiteAPIEndToEndTests/FoodybiteNetworkingAPIEndToEndTests.swift
@@ -0,0 +1,52 @@
+//
+//  FoodybiteNetworkingAPIEndToEndTests.swift
+//  FoodybiteAPIEndToEndTests
+//
+//  Created by Marian Stanciulica on 26.01.2023.
+//
+
+import XCTest
+import DomainModels
+import FoodybiteNetworking
+
+final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
+
+    func test_endToEndSignUp_returnsSuccesfully() async {
+        let httpClient = URLSessionHTTPClient()
+        let tokenStore = KeychainTokenStore()
+        
+        let remoteResourceLoader = RemoteResourceLoader(client: httpClient)
+        let apiService = APIService(loader: remoteResourceLoader,
+                                    sender: remoteResourceLoader,
+                                    tokenStore: tokenStore)
+        
+        do {
+            try await apiService.signUp(name: testingName,
+                                        email: testingEmail,
+                                        password: testingPassword,
+                                        confirmPassword: testingPassword,
+                                        profileImage: testingProfileImage)
+        } catch {
+            XCTFail("Expected successful sign up request, got \(error) instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private var testingName: String {
+        "Testing"
+    }
+    
+    private var testingEmail: String {
+        "testing@testing.com"
+    }
+    
+    private var testingPassword: String {
+        "123Password321$"
+    }
+    
+    private var testingProfileImage: Data? {
+        "any data".data(using: .utf8)
+    }
+    
+}
