e27902190fa22ab3b889fcf4134ebaf19ee75714
Marian25 <marian.stanciulica@gmail.com>
2023-01-26 09:23:28 +0200

    Autocomplete response updated with all fields

--- FoodybitePlaces/Response Models/Autocomplete/AutocompleteResponse.swift
+++ FoodybitePlaces/Response Models/Autocomplete/AutocompleteResponse.swift
@@ -5,6 +5,47 @@
 //  Created by Marian Stanciulica on 02.01.2023.
 //
 
-struct AutocompleteResponse: Decodable {
-    let predictions: [AutocompletePrediction]
+struct AutocompleteResponse: Codable {
+    let predictions: [Prediction]
+    let status: String
+}
+
+struct Prediction: Codable {
+    let description: String
+    let matchedSubstrings: [MatchedSubstring]
+    let placeID: String
+    let reference: String
+    let structuredFormatting: StructuredFormatting
+    let terms: [Term]
+    let types: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case description
+        case matchedSubstrings = "matched_substrings"
+        case placeID = "place_id"
+        case reference
+        case structuredFormatting = "structured_formatting"
+        case terms, types
+    }
+}
+
+struct MatchedSubstring: Codable {
+    let length, offset: Int
+}
+
+struct StructuredFormatting: Codable {
+    let mainText: String
+    let mainTextMatchedSubstrings: [MatchedSubstring]
+    let secondaryText: String
+
+    enum CodingKeys: String, CodingKey {
+        case mainText = "main_text"
+        case mainTextMatchedSubstrings = "main_text_matched_substrings"
+        case secondaryText = "secondary_text"
+    }
+}
+
+struct Term: Codable {
+    let offset: Int
+    let value: String
 }
