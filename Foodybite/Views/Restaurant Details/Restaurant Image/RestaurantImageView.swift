227db53b387a7ef40d27517bc9e49f7c45a71a0c
Marian25 <marian.stanciulica@gmail.com>
2023-01-28 14:43:57 +0200

    Remove unnecessary views from RestaurantImageView

--- Foodybite/Views/Restaurant Details/Restaurant Image/RestaurantImageView.swift
+++ Foodybite/Views/Restaurant Details/Restaurant Image/RestaurantImageView.swift
@@ -9,36 +9,10 @@ import SwiftUI
 
 struct RestaurantImageView: View {
     var body: some View {
-        ZStack(alignment: .top) {
+        ZStack {
             Image("restaurant_logo_test")
                 .resizable()
                 .aspectRatio(1.2, contentMode: .fit)
-
-            HStack {
-                Spacer()
-
-                Button {
-
-                } label: {
-                    Image(systemName: "square.and.arrow.up")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundColor(.white)
-                }
-                .frame(width: 32, height: 32)
-
-                Button {
-
-                } label: {
-                    Image(systemName: "star")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .foregroundColor(.white)
-                }
-                .frame(width: 32, height: 32)
-                .padding()
-            }
-            .padding()
 
             VStack {
                 Spacer()
@@ -53,5 +27,6 @@ struct RestaurantImageView: View {
 struct RestaurantImageView_Previews: PreviewProvider {
     static var previews: some View {
         RestaurantImageView()
+            .background(.black)
     }
 }
