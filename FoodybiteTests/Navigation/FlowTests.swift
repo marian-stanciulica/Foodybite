5c7a3361d2f1335c3df4bca96eea57332a63975d
Marian25 <marian.stanciulica@gmail.com>
2023-01-28 13:47:25 +0200

    'FlowTests' updated to increase coverage

--- FoodybiteTests/Navigation/FlowTests.swift
+++ FoodybiteTests/Navigation/FlowTests.swift
@@ -10,6 +10,14 @@ import Foodybite
 
 final class FlowTests: XCTestCase {
     
+    func test_authRoute_containsAllCases() {
+        XCTAssertEqual(AuthRoute.allCases, [.signUp])
+    }
+    
+    func test_profileRoute_containsAllCases() {
+        XCTAssertEqual(ProfileRoute.allCases, [.settings, .changePassword, .editProfile])
+    }
+    
     func test_append_appendsValueToNavigationPath() {
         let sut = Flow<AuthRoute>()
         
@@ -20,12 +28,18 @@ final class FlowTests: XCTestCase {
     }
     
     func test_navigateBack_removesLastValueFromNavigationPath() {
-        let sut = Flow<AuthRoute>()
+        let sut = Flow<ProfileRoute>()
         
-        sut.append(.signUp)
-        XCTAssertEqual(sut.path.count, 1)
+        sut.append(.settings)
+        XCTAssertEqual(sut.path, [.settings])
+        
+        sut.append(.changePassword)
+        XCTAssertEqual(sut.path, [.settings, .changePassword])
         
         sut.navigateBack()
-        XCTAssertEqual(sut.path.count, 0)
+        XCTAssertEqual(sut.path, [.settings])
+        
+        sut.navigateBack()
+        XCTAssertTrue(sut.path.isEmpty)
     }
 }
