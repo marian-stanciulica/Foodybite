96747553b75fb1859ef6b45781be3322fbc17d07
Marian25 <marian.stanciulica@gmail.com>
2023-01-29 13:22:44 +0200

    FetchPlacePhotoService added

---
+++ DomainModels/Places Protocols/FetchPlacePhotoService.swift
@@ -0,0 +1,12 @@
+//
+//  FetchPlacePhotoService.swift
+//  DomainModels
+//
+//  Created by Marian Stanciulica on 29.01.2023.
+//
+
+import Foundation
+
+public protocol FetchPlacePhotoService {
+    func fetchPlacePhoto(photoReference: String) async throws -> Data
+}
