c57ebd75b6d5c209103039242a6bdc27e66e784c
Marian25 <marian.stanciulica@gmail.com>
2023-02-03 18:45:09 +0200

    TabNavigationViewModel - getCurrentLocation requests location from provider

--- FoodybiteTests/Presentation/TabNavigationView/TabNavigationViewModelTests.swift
+++ FoodybiteTests/Presentation/TabNavigationView/TabNavigationViewModelTests.swift
@@ -6,27 +6,60 @@
 //
 
 import XCTest
+import DomainModels
 
 final class TabNavigationViewModel {
     enum State {
         case isLoading
     }
     
+    private let locationProvider: LocationProviding
     var state: State = .isLoading
     
-    func getCurrentLocation() {
+    init(locationProvider: LocationProviding) {
+        self.locationProvider = locationProvider
+    }
+    
+    func getCurrentLocation() async {
         state = .isLoading
+        _ = try? await locationProvider.requestLocation()
     }
 }
 
 final class TabNavigationViewModelTests: XCTestCase {
     
-    func test_getCurrentLocation_setsStatetoLoading() {
-        let sut = TabNavigationViewModel()
+    func test_getCurrentLocation_setsStateToLoading() async {
+        let (sut, _) = makeSUT()
         
-        sut.getCurrentLocation()
+        await sut.getCurrentLocation()
         
         XCTAssertEqual(sut.state, .isLoading)
+    }
+    
+    func test_getCurrentLocation_callsLocationProvider() async {
+        let (sut, locationProviderSpy) = makeSUT()
+        
+        await sut.getCurrentLocation()
+        
+        XCTAssertEqual(locationProviderSpy.requestLocationCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: TabNavigationViewModel, locationProviderSpy: LocationProvidingSpy) {
+        let locationProviderSpy = LocationProvidingSpy()
+        let sut = TabNavigationViewModel(locationProvider: locationProviderSpy)
+        return (sut, locationProviderSpy)
+    }
+    
+    private class LocationProvidingSpy: LocationProviding {
+        var locationServicesEnabled: Bool = false
+        private(set) var requestLocationCallCount = 0
+        
+        func requestLocation() async throws -> Location {
+            requestLocationCallCount += 1
+            throw NSError(domain: "any error", code: 1)
+        }
     }
     
 }
