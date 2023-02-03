95ec92c81a4d1e21c705a3f32c647235e0e2d210
Marian25 <marian.stanciulica@gmail.com>
2023-02-03 17:35:30 +0200

    'LocationProviding' protocol added to domain

---
+++ DomainModels/Location Protocols/LocationProviding.swift
@@ -0,0 +1,13 @@
+//
+//  LocationProviding.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 03.02.2023.
+//
+
+
+public protocol LocationProviding {
+    var locationServicesEnabled: Bool { get }
+    
+    func requestLocation() async throws -> Location
+}
