2c64a83cfff8cb64d5ca5cafee606f2286b4880e
Marian25 <marian.stanciulica@gmail.com>
2023-02-04 15:59:23 +0200

    ReviewEndpointTests - makeSUT renamed to makeAddReviewSUT

--- FoodybiteNetworkingTests/Endpoint/ReviewEndpointTests.swift
+++ FoodybiteNetworkingTests/Endpoint/ReviewEndpointTests.swift
@@ -11,32 +11,32 @@ import FoodybiteNetworking
 final class ReviewEndpointTests: XCTestCase {
     
     func test_addReview_baseURL() {
-        XCTAssertEqual(makeSUT().host, "localhost")
+        XCTAssertEqual(makeAddReviewSUT().host, "localhost")
     }
     
     func test_addReview_path() {
-        XCTAssertEqual(makeSUT().path, "/review")
+        XCTAssertEqual(makeAddReviewSUT().path, "/review")
     }
     
     func test_addReview_methodIsPost() {
-        XCTAssertEqual(makeSUT().method, .post)
+        XCTAssertEqual(makeAddReviewSUT().method, .post)
     }
     
     func test_addReview_body() throws {
         let body = AddReviewRequest(placeID: anyPlaceID(), text: anyReviewText(), stars: anyStarsNumber())
-        let sut = makeSUT(body: body)
+        let sut = makeAddReviewSUT(body: body)
         let receivedBody = try XCTUnwrap(sut.body as? AddReviewRequest)
         
         XCTAssertEqual(receivedBody, body)
     }
     
     func test_addReview_headersContainContentTypeJSON() {
-        XCTAssertEqual(makeSUT().headers["Content-Type"], "application/json")
+        XCTAssertEqual(makeAddReviewSUT().headers["Content-Type"], "application/json")
     }
     
     // MARK: - Helpers
     
-    private func makeSUT(body: AddReviewRequest? = nil) -> ReviewEndpoint {
+    private func makeAddReviewSUT(body: AddReviewRequest? = nil) -> ReviewEndpoint {
         let defaultBody = AddReviewRequest(placeID: anyPlaceID(), text: anyReviewText(), stars: anyStarsNumber())
         return .addReview(body ?? defaultBody)
     }
