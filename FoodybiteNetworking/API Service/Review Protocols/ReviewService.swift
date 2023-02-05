062e0b48f5a0bb19bc0a0a4c663ef666b9f27e02
Marian25 <marian.stanciulica@gmail.com>
2023-02-05 11:39:23 +0200

    'getReviews' method added to ReviewService && APIService conforms to ReviewService

--- FoodybiteNetworking/API Service/Review Protocols/ReviewService.swift
+++ FoodybiteNetworking/API Service/Review Protocols/ReviewService.swift
@@ -5,6 +5,9 @@
 //  Created by Marian Stanciulica on 04.02.2023.
 //
 
+import DomainModels
+
 public protocol ReviewService {
     func addReview(placeID: String, reviewText: String, starsNumber: Int) async throws
+    func getReviews(placeID: String?) async throws -> [Review]
 }
