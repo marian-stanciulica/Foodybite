1733379c845efa2249672fb82f750b35b97e27cd
Marian25 <marian.stanciulica@gmail.com>
2023-02-09 13:45:47 +0200

    RestaurantReviewCellViewModel - getPlaceDetails sets state to .requestSucceeeded when 'GetPlaceDetailsService' returns successfully

--- FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
+++ FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
@@ -12,6 +12,7 @@ class RestaurantReviewCellViewModel: ObservableObject {
     enum State: Equatable {
         case isLoading
         case loadingError(String)
+        case requestSucceeeded(PlaceDetails)
     }
     
     private let placeID: String
@@ -26,7 +27,8 @@ class RestaurantReviewCellViewModel: ObservableObject {
     
     func getPlaceDetails() async {
         do {
-            _ = try await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
+            let placeDetails = try await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
+            getPlaceDetailsState = .requestSucceeeded(placeDetails)
         } catch {
             getPlaceDetailsState = .loadingError("An error occured while fetching review details. Please try again later!")
         }
@@ -61,6 +63,17 @@ final class RestaurantReviewCellViewModelTests: XCTestCase {
         XCTAssertEqual(stateSpy.results, [.isLoading, .loadingError("An error occured while fetching review details. Please try again later!")])
     }
     
+    func test_getPlaceDetails_setsStateToRequestSucceeededWhenGetPlaceDetailsServiceReturnsSuccessfully() async {
+        let (sut, getPlaceDetailsServiceSpy) = makeSUT()
+        let expectedPlaceDetails = anyPlaceDetails()
+        getPlaceDetailsServiceSpy.result = .success(expectedPlaceDetails)
+        let stateSpy = PublisherSpy(sut.$getPlaceDetailsState.eraseToAnyPublisher())
+
+        await sut.getPlaceDetails()
+        
+        XCTAssertEqual(stateSpy.results, [.isLoading, .requestSucceeeded(expectedPlaceDetails)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(placeID: String = "") -> (sut: RestaurantReviewCellViewModel,
@@ -76,6 +89,51 @@ final class RestaurantReviewCellViewModelTests: XCTestCase {
     
     private func anyError() -> NSError {
         NSError(domain: "any error", code: 1)
+    }
+    
+    private func anyPlaceDetails() -> PlaceDetails {
+        PlaceDetails(
+            phoneNumber: "+61 2 9374 4000",
+            name: "Place name",
+            address: "48 Pirrama Rd, Pyrmont NSW 2009, Australia",
+            rating: rating().raw,
+            openingHoursDetails: OpeningHoursDetails(
+                openNow: true,
+                weekdayText: [
+                    "Monday: 9:00 AM – 5:00 PM",
+                    "Tuesday: 9:00 AM – 5:00 PM",
+                    "Wednesday: 9:00 AM – 5:00 PM",
+                    "Thursday: 9:00 AM – 5:00 PM",
+                    "Friday: 9:00 AM – 5:00 PM",
+                    "Saturday: Closed",
+                    "Sunday: Closed",
+                ]
+            ),
+            reviews: [
+                Review(
+                    profileImageURL: URL(string: "www.google.com"),
+                    profileImageData: nil,
+                    authorName: "Marian",
+                    reviewText: "Loren ipsum...",
+                    rating: 2,
+                    relativeTime: "5 months ago"
+                )
+            ],
+            location: Location(latitude: 4.4, longitude: 6.9),
+            photos: anyPhotos()
+        )
+    }
+    
+    private func rating() -> (raw: Double, formatted: String) {
+        (4.52, "4.5")
+    }
+    
+    private func anyPhotos() -> [Photo] {
+        [
+            Photo(width: 100, height: 200, photoReference: "first photo reference"),
+            Photo(width: 200, height: 300, photoReference: "second photo reference"),
+            Photo(width: 300, height: 400, photoReference: "third photo reference")
+        ]
     }
     
     private class GetPlaceDetailsServiceSpy: GetPlaceDetailsService {
