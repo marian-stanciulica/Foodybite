27f570edb99f221b9cc80249da2c9050861af381
Marian25 <marian.stanciulica@gmail.com>
2023-02-09 14:37:38 +0200

    Move 'RestaurantReviewCellViewModel' to production

---
+++ Foodybite/Presentation/Profile/RestaurantReviewCellViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  RestaurantReviewCellViewModel.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 09.02.2023.
+//
+
+import Foundation
+import Domain
+
+public class RestaurantReviewCellViewModel: ObservableObject {
+    public enum State<T>: Equatable where T: Equatable {
+        case isLoading
+        case loadingError(String)
+        case requestSucceeeded(T)
+    }
+    
+    private let placeID: String
+    private let getPlaceDetailsService: GetPlaceDetailsService
+    private let fetchPlacePhotoService: FetchPlacePhotoService
+    
+    @Published public var getPlaceDetailsState: State<PlaceDetails> = .isLoading
+    @Published public var fetchPhotoState: State<Data> = .isLoading
+    
+    public var rating: String {
+        if case let .requestSucceeeded(placeDetails) = getPlaceDetailsState {
+            return String(format: "%.1f", placeDetails.rating)
+        }
+        
+        return ""
+    }
+    
+    public init(placeID: String, getPlaceDetailsService: GetPlaceDetailsService, fetchPlacePhotoService: FetchPlacePhotoService) {
+        self.placeID = placeID
+        self.getPlaceDetailsService = getPlaceDetailsService
+        self.fetchPlacePhotoService = fetchPlacePhotoService
+    }
+    
+    public func getPlaceDetails() async {
+        do {
+            let placeDetails = try await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
+            getPlaceDetailsState = .requestSucceeeded(placeDetails)
+            
+            if let firstPhoto = placeDetails.photos.first {
+                await fetchPhoto(firstPhoto)
+            }
+        } catch {
+            getPlaceDetailsState = .loadingError("An error occured while fetching review details. Please try again later!")
+        }
+    }
+    
+    private func fetchPhoto(_ photo: Photo) async {
+        do {
+            let photoData = try await fetchPlacePhotoService.fetchPlacePhoto(photoReference: photo.photoReference)
+            fetchPhotoState = .requestSucceeeded(photoData)
+        } catch {
+            fetchPhotoState = .loadingError("An error occured while fetching place photo. Please try again later!")
+        }
+    }
+}
--- FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
+++ FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
@@ -7,57 +7,7 @@
 
 import XCTest
 import Domain
-
-class RestaurantReviewCellViewModel: ObservableObject {
-    enum State<T>: Equatable where T: Equatable {
-        case isLoading
-        case loadingError(String)
-        case requestSucceeeded(T)
-    }
-    
-    private let placeID: String
-    private let getPlaceDetailsService: GetPlaceDetailsService
-    private let fetchPlacePhotoService: FetchPlacePhotoService
-    
-    @Published public var getPlaceDetailsState: State<PlaceDetails> = .isLoading
-    @Published public var fetchPhotoState: State<Data> = .isLoading
-    
-    public var rating: String {
-        if case let .requestSucceeeded(placeDetails) = getPlaceDetailsState {
-            return String(format: "%.1f", placeDetails.rating)
-        }
-        
-        return ""
-    }
-    
-    init(placeID: String, getPlaceDetailsService: GetPlaceDetailsService, fetchPlacePhotoService: FetchPlacePhotoService) {
-        self.placeID = placeID
-        self.getPlaceDetailsService = getPlaceDetailsService
-        self.fetchPlacePhotoService = fetchPlacePhotoService
-    }
-    
-    func getPlaceDetails() async {
-        do {
-            let placeDetails = try await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
-            getPlaceDetailsState = .requestSucceeeded(placeDetails)
-            
-            if let firstPhoto = placeDetails.photos.first {
-                await fetchPhoto(firstPhoto)
-            }
-        } catch {
-            getPlaceDetailsState = .loadingError("An error occured while fetching review details. Please try again later!")
-        }
-    }
-    
-    private func fetchPhoto(_ photo: Photo) async {
-        do {
-            let photoData = try await fetchPlacePhotoService.fetchPlacePhoto(photoReference: photo.photoReference)
-            fetchPhotoState = .requestSucceeeded(photoData)
-        } catch {
-            fetchPhotoState = .loadingError("An error occured while fetching place photo. Please try again later!")
-        }
-    }
-}
+import Foodybite
 
 final class RestaurantReviewCellViewModelTests: XCTestCase {
     
