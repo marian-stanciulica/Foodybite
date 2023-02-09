86d5fefccd355ce0c0c3048644678b8b02b2cd4e
Marian25 <marian.stanciulica@gmail.com>
2023-02-09 13:37:24 +0200

    RestaurantReviewCellViewModel - getPlaceDetails sets state on loadingError when 'GetPlaceDetailsService' throws error

--- FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
+++ FoodybiteTests/Presentation/Profile/RestaurantReviewCellViewModelTests.swift
@@ -8,9 +8,16 @@
 import XCTest
 import Domain
 
-class RestaurantReviewCellViewModel {
+class RestaurantReviewCellViewModel: ObservableObject {
+    enum State: Equatable {
+        case isLoading
+        case loadingError(String)
+    }
+    
     private let placeID: String
     private let getPlaceDetailsService: GetPlaceDetailsService
+    
+    @Published public var getPlaceDetailsState: State = .isLoading
     
     init(placeID: String, getPlaceDetailsService: GetPlaceDetailsService) {
         self.placeID = placeID
@@ -18,7 +25,11 @@ class RestaurantReviewCellViewModel {
     }
     
     func getPlaceDetails() async {
-        _ = try? await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
+        do {
+            _ = try await getPlaceDetailsService.getPlaceDetails(placeID: placeID)
+        } catch {
+            getPlaceDetailsState = .loadingError("An error occured while fetching review details. Please try again later!")
+        }
     }
 }
 
@@ -34,6 +45,16 @@ final class RestaurantReviewCellViewModelTests: XCTestCase {
         XCTAssertEqual(getPlaceDetailsServiceSpy.capturedValues.first, anyPlaceID)
     }
     
+    func test_getPlaceDetails_setsStateToLoadingErrorWhenGetPlaceDetailsServiceThrowsError() async {
+        let (sut, getPlaceDetailsServiceSpy) = makeSUT()
+        getPlaceDetailsServiceSpy.result = .failure(anyError())
+        let stateSpy = PublisherSpy(sut.$getPlaceDetailsState.eraseToAnyPublisher())
+
+        await sut.getPlaceDetails()
+        
+        XCTAssertEqual(stateSpy.results, [.isLoading, .loadingError("An error occured while fetching review details. Please try again later!")])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(placeID: String = "") -> (sut: RestaurantReviewCellViewModel,
@@ -47,16 +68,30 @@ final class RestaurantReviewCellViewModelTests: XCTestCase {
         "any place id"
     }
     
-    private var anyError: NSError {
+    private func anyError() -> NSError {
         NSError(domain: "any error", code: 1)
     }
     
     private class GetPlaceDetailsServiceSpy: GetPlaceDetailsService {
         private(set) var capturedValues = [String]()
+        var result: Result<PlaceDetails, Error>?
         
         func getPlaceDetails(placeID: String) async throws -> PlaceDetails {
             capturedValues.append(placeID)
-            return PlaceDetails(phoneNumber: nil, name: "", address: "", rating: 0, openingHoursDetails: nil, reviews: [], location: Location(latitude: 0, longitude: 0), photos: [])
+            
+            if let result = result {
+                return try result.get()
+            }
+            
+            return PlaceDetails(phoneNumber: nil,
+                                name: "",
+                                address: "",
+                                rating: 0,
+                                openingHoursDetails: nil,
+                                reviews: [],
+                                location: Location(latitude: 0, longitude: 0),
+                                photos: []
+            )
         }
     }
     
