5d10323d083be8798d0f234701f716f201429241
Marian25 <marian.stanciulica@gmail.com>
2023-02-10 10:08:41 +0200

    AutocompletePrediction added in Domain module

---
+++ Domain/Models/Autocomplete Places/AutocompletePrediction.swift
@@ -0,0 +1,11 @@
+//
+//  AutocompletePrediction.swift
+//  Domain
+//
+//  Created by Marian Stanciulica on 10.02.2023.
+//
+
+public struct AutocompletePrediction {
+    public let placePrediction: String
+    public let placeID: String
+}
