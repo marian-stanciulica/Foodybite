40f51bf6a9e52a4f9b955ea400fbc2c6ce4357da
Marian25 <marian.stanciulica@gmail.com>
2023-02-11 17:59:57 +0200

    Move 'SelectedRestaurantViewModel' to production

---
+++ Foodybite/Presentation/New Review/SelectedRestaurantViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  SelectedRestaurantViewModel.swift
+//  Foodybite
+//
+//  Created by Marian Stanciulica on 11.02.2023.
+//
+
+import Foundation
+import Domain
+
+public final class SelectedRestaurantViewModel {
+    public enum State: Equatable {
+        case idle
+        case isLoading
+        case loadingError(String)
+        case requestSucceeeded(Data)
+    }
+    
+    private let placeDetails: PlaceDetails
+    private let fetchPlacePhotoService: FetchPlacePhotoService
+    
+    public var placeName: String {
+        placeDetails.name
+    }
+    
+    public var placeAddress: String {
+        placeDetails.address
+    }
+    
+    @Published public var fetchPhotoState: State = .idle
+    
+    public init(placeDetails: PlaceDetails, fetchPlacePhotoService: FetchPlacePhotoService) {
+        self.placeDetails = placeDetails
+        self.fetchPlacePhotoService = fetchPlacePhotoService
+    }
+    
+    public func fetchPhoto() async {
+        fetchPhotoState = .isLoading
+        
+        guard let firstPhoto = placeDetails.photos.first else {
+            fetchPhotoState = .loadingError("This place has no photos!")
+            return
+        }
+        
+        do {
+            let photoData = try await fetchPlacePhotoService.fetchPlacePhoto(photoReference: firstPhoto.photoReference)
+            fetchPhotoState = .requestSucceeeded(photoData)
+        } catch {
+            fetchPhotoState = .loadingError("An error occured while fetching place photo. Please try again later!")
+        }
+    }
+}
+
--- FoodybiteTests/Presentation/New Review/SelectedRestaurantViewModelTests.swift
+++ FoodybiteTests/Presentation/New Review/SelectedRestaurantViewModelTests.swift
@@ -7,49 +7,7 @@
 
 import XCTest
 import Domain
-
-final class SelectedRestaurantViewModel {
-    public enum State: Equatable {
-        case idle
-        case isLoading
-        case loadingError(String)
-        case requestSucceeeded(Data)
-    }
-    
-    private let placeDetails: PlaceDetails
-    private let fetchPlacePhotoService: FetchPlacePhotoService
-    
-    public var placeName: String {
-        placeDetails.name
-    }
-    
-    public var placeAddress: String {
-        placeDetails.address
-    }
-    
-    @Published public var fetchPhotoState: State = .idle
-    
-    init(placeDetails: PlaceDetails, fetchPlacePhotoService: FetchPlacePhotoService) {
-        self.placeDetails = placeDetails
-        self.fetchPlacePhotoService = fetchPlacePhotoService
-    }
-    
-    public func fetchPhoto() async {
-        fetchPhotoState = .isLoading
-        
-        guard let firstPhoto = placeDetails.photos.first else {
-            fetchPhotoState = .loadingError("This place has no photos!")
-            return
-        }
-        
-        do {
-            let photoData = try await fetchPlacePhotoService.fetchPlacePhoto(photoReference: firstPhoto.photoReference)
-            fetchPhotoState = .requestSucceeeded(photoData)
-        } catch {
-            fetchPhotoState = .loadingError("An error occured while fetching place photo. Please try again later!")
-        }
-    }
-}
+import Foodybite
 
 final class SelectedRestaurantViewModelTests: XCTestCase {
     
