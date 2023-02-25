98712ff30ad7a2024885fdb52124b63ad9f08776
Marian25 <marian.stanciulica@gmail.com>
2023-02-25 18:49:23 +0200

    LoginViewSnapshotTests - adds snapshot test for idle state

---
+++ FoodybiteTests/Snapshot Tests/Login/LoginViewSnapshotTests.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewSnapshotTests.swift
+//  FoodybiteTests
+//
+//  Created by Marian Stanciulica on 25.02.2023.
+//
+
+import XCTest
+import SwiftUI
+import SnapshotTesting
+import Domain
+@testable import Foodybite
+
+final class LoginViewSnapshotTests: XCTestCase {
+ 
+    func test_loginViewIdleState() {
+        let sut = makeSUT()
+        
+        assertSnapshot(matching: sut, as: .image(on: .iPhone13))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(email: String = "", password: String = "") -> UIViewController {
+        let viewModel = LoginViewModel(loginService: EmptyLoginService(), goToMainTab: { _ in })
+        viewModel.email = email
+        viewModel.password = password
+        let registerView = LoginView(viewModel: viewModel, goToSignUp: {})
+        let sut = UIHostingController(rootView: registerView)
+        return sut
+    }
+    
+    private class EmptyLoginService: LoginService {
+        func login(email: String, password: String) async throws -> User {
+            User(id: UUID(), name: "", email: "", profileImage: nil)
+        }
+    }
+}
