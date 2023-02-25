dded9c79c9fd83d1793b867f29b88828490e82a8
Marian25 <marian.stanciulica@gmail.com>
2023-02-25 16:42:49 +0200

    RegisterViewSnapshotTests - add makeSUT helper method to remove duplication

--- FoodybiteTests/Snapshot Tests/Register/RegisterViewSnapshotTests.swift
+++ FoodybiteTests/Snapshot Tests/Register/RegisterViewSnapshotTests.swift
@@ -14,14 +14,19 @@ import Domain
 final class RegisterViewSnapshotTests: XCTestCase {
     
     func test_defaultAppearance() {
-        let viewModel = RegisterViewModel(signUpService: EmptySignUpService())
-        let registerView = RegisterView(viewModel: viewModel, goToLogin: {})
-        let controller = UIHostingController(rootView: registerView)
+        let sut = makeSUT()
         
-        assertSnapshot(matching: controller, as: .image(on: .iPhone13))
+        assertSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> UIViewController {
+        let viewModel = RegisterViewModel(signUpService: EmptySignUpService())
+        let registerView = RegisterView(viewModel: viewModel, goToLogin: {})
+        let sut = UIHostingController(rootView: registerView)
+        return sut
+    }
     
     private class EmptySignUpService: SignUpService {
         func signUp(name: String, email: String, password: String, confirmPassword: String, profileImage: Data?) async throws {}
