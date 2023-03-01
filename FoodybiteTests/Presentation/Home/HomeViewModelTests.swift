c50bb250a9453f465162a41c5efec51511effe23
Marian25 <marian.stanciulica@gmail.com>
2023-03-01 11:51:27 +0200

    HomeViewModel - tests updated to check if the search nearby state goes through the .isLoading state

--- FoodybiteTests/Presentation/Home/HomeViewModelTests.swift
+++ FoodybiteTests/Presentation/Home/HomeViewModelTests.swift
@@ -11,21 +11,25 @@ import Foodybite
 
 final class HomeViewModelTests: XCTestCase {
     
+    func test_init_searchNearbyState() {
+        let (sut, _) = makeSUT()
+
+        XCTAssertEqual(sut.searchNearbyState, .idle)
+    }
+    
     func test_searchNearby_setsErrorWhenSearchNearbyServiceThrowsError() async {
         let (sut, serviceSpy) = makeSUT()
-        
         serviceSpy.result = .failure(anyError)
-        await sut.searchNearby()
-        XCTAssertEqual(sut.searchNearbyState, .failure(.connectionFailure))
+        
+        await assert(on: sut, withExpectedResult: .failure(.connectionFailure))
     }
     
     func test_searchNearby_updatesNearbyPlacesWhenSearchNearbyServiceReturnsSuccessfully() async {
         let (sut, serviceSpy) = makeSUT()
         let expectedNearbyPlaces = anyNearbyPlaces
-        
         serviceSpy.result = .success(expectedNearbyPlaces)
-        await sut.searchNearby()
-        XCTAssertEqual(sut.searchNearbyState, .success(expectedNearbyPlaces))
+        
+        await assert(on: sut, withExpectedResult: .success(expectedNearbyPlaces))
     }
     
     // MARK: - Helpers
@@ -34,6 +38,20 @@ final class HomeViewModelTests: XCTestCase {
         let serviceSpy = SearchNearbyServiceSpy()
         let sut = HomeViewModel(searchNearbyService: serviceSpy, currentLocation: anyLocation)
         return (sut, serviceSpy)
+    }
+    
+    private func assert(on sut: HomeViewModel,
+                        withExpectedResult expectedResult: HomeViewModel.State,
+                        file: StaticString = #file,
+                        line: UInt = #line) async {
+        let resultSpy = PublisherSpy(sut.$searchNearbyState.eraseToAnyPublisher())
+
+        XCTAssertEqual(resultSpy.results, [.idle], file: file, line: line)
+        
+        await sut.searchNearby()
+        
+        XCTAssertEqual(resultSpy.results, [.idle, .isLoading, expectedResult], file: file, line: line)
+        resultSpy.cancel()
     }
     
     private var anyError: NSError {
