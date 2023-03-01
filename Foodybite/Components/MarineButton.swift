8b2e1fa61621a7e27168f0e67748172b7ca6b54a
Marian25 <marian.stanciulica@gmail.com>
2023-03-01 16:02:30 +0200

    Update MarineButton to detect tap gesture in its bounds

--- Foodybite/Components/MarineButton.swift
+++ Foodybite/Components/MarineButton.swift
@@ -22,33 +22,30 @@ struct MarineButton: View {
                     .padding(2)
             }
             
-            Button(action: action) {
-                Text(title)
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .disabled(isLoading)
-            }
-            .padding(.vertical)
+            Text(title)
+                .foregroundColor(.white)
+                .font(.headline)
+                .disabled(isLoading)
+                .padding(.vertical)
             
             Spacer()
         }
         .background(isLoading ? Color.marineBlue.opacity(0.5) : Color.marineBlue)
         .cornerRadius(16)
         .frame(maxWidth: .infinity, minHeight: 48)
+        .onTapGesture {
+            action()
+        }
     }
 }
 
 struct RoundedButton_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
-            MarineButton(title: "Title", isLoading: true) {
-                
-            }
+            MarineButton(title: "Title", isLoading: true) {}
             .padding(.vertical)
             
-            MarineButton(title: "Title", isLoading: false) {
-                
-            }
+            MarineButton(title: "Title", isLoading: false) {}
         }
     }
 }
