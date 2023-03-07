b9d9966d2924c3bfd2b7394caf442dc4259b00e9
Marian25 <marian.stanciulica@gmail.com>
2023-03-07 14:36:15 +0200

    Adds 'ManagedNearbyPlace' model to CoreData store

---
+++ FoodybitePersistence/Cache/Infrastructure/Models/ManagedNearbyPlace.swift
@@ -0,0 +1,35 @@
+//
+//  ManagedNearbyPlace.swift
+//  FoodybitePersistence
+//
+//  Created by Marian Stanciulica on 07.03.2023.
+//
+
+import Foundation
+import CoreData
+import Domain
+
+@objc(ManagedNearbyPlace)
+public class ManagedNearbyPlace: NSManagedObject {
+    @NSManaged public var placeID: String
+    @NSManaged public var placeName: String
+    @NSManaged public var isOpen: Bool
+    @NSManaged public var rating: Double
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ManagedNearbyPlace> {
+        return NSFetchRequest<ManagedNearbyPlace>(entityName: "ManagedNearbyPlace")
+    }
+    
+    public convenience init(_ model: NearbyPlace, for context: NSManagedObjectContext) {
+        self.init(context: context)
+
+        self.placeID = model.placeID
+        self.placeName = model.placeName
+        self.isOpen = model.isOpen
+        self.rating = model.rating
+        self.latitude = model.location.latitude
+        self.longitude = model.location.longitude
+    }
+}
