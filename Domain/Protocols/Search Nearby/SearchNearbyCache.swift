9d094e69d81b8fe17b060fbf933bbde385561849
Marian25 <marian.stanciulica@gmail.com>
2023-03-07 18:30:00 +0200

    Adds 'SearchNearbyCache' protocol to Domain module

---
+++ Domain/Protocols/Search Nearby/SearchNearbyCache.swift
@@ -0,0 +1,10 @@
+//
+//  SearchNearbyCache.swift
+//  Domain
+//
+//  Created by Marian Stanciulica on 07.03.2023.
+//
+
+public protocol SearchNearbyCache {
+    func save(nearbyPlaces: [NearbyPlace]) async throws
+}
