dc03f21bd88981dcdec2bcdcd7b02b76d3a3cb25
Marian25 <marian.stanciulica@gmail.com>
2023-03-08 15:58:17 +0200

    GetPlaceDetailsDAO - getPlaceDetails throws error when store throws error

--- FoodybitePersistenceTests/Cache/Get Place Details/GetPlaceDetailsDAOTests.swift
+++ FoodybitePersistenceTests/Cache/Get Place Details/GetPlaceDetailsDAOTests.swift
@@ -9,15 +9,31 @@ import XCTest
 import Domain
 import FoodybitePersistence
 
-public final class GetPlaceDetailsDAO {
+public final class GetPlaceDetailsDAO: GetPlaceDetailsService {
     private let store: LocalStoreReader & LocalStoreWriter
 
     init(store: LocalStoreReader & LocalStoreWriter) {
         self.store = store
     }
+    
+    public func getPlaceDetails(placeID: String) async throws -> PlaceDetails {
+        return try await store.read()
+    }
 }
 
 final class GetPlaceDetailsDAOTests: XCTestCase {
+    
+    func test_getPlaceDetails_throwsErrorWhenStoreThrowsError() async {
+        let (sut, storeSpy) = makeSUT()
+        storeSpy.readResult = .failure(anyError())
+        
+        do {
+            let nearbyPlaces = try await sut.getPlaceDetails(placeID: "place #1")
+            XCTFail("Expected to fail, received nearby places \(nearbyPlaces) instead")
+        } catch {
+            XCTAssertNotNil(error)
+        }
+    }
     
     // MARK: - Helpers
     
