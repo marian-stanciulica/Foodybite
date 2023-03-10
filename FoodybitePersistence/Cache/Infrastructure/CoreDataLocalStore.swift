307e2b1707a0e013026af01a606c2b77cf754486
Marian25 <marian.stanciulica@gmail.com>
2023-03-10 13:42:30 +0200

    CoreDataLocalStore - set mergePolicy to 'NSMergeByPropertyObjectTrumpMergePolicy' and remove the deletion of all objects on write and writeAll

--- FoodybitePersistence/Cache/Infrastructure/CoreDataLocalStore.swift
+++ FoodybitePersistence/Cache/Infrastructure/CoreDataLocalStore.swift
@@ -16,6 +16,7 @@ public class CoreDataLocalStore: LocalStore {
     public init(storeURL: URL, bundle: Bundle = .init(for: CoreDataLocalStore.self)) throws {
         let container = try NSPersistentContainer.load(modelName: "Store", bundle: bundle, storeURL: storeURL)
         context = container.newBackgroundContext()
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
     }
     
     public func read<T: LocalModelConvertable>() async throws -> T {
@@ -40,15 +41,8 @@ public class CoreDataLocalStore: LocalStore {
     
     public func write<T: LocalModelConvertable>(_ object: T) async throws {
         try await context.perform {
-            let request = T.LocalModel.fetchRequest()
-            
-            if let results = try self.context.fetch(request) as? [T.LocalModel] {
-                for object in results {
-                    self.context.delete(object)
-                }
-            }
-            
-            self.context.insert(object.toLocalModel(context: self.context))
+            let local = object.toLocalModel(context: self.context)
+            self.context.insert(local)
             try self.context.save()
         }
     }
