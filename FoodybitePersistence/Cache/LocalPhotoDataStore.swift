6e58e1f733fa3903c4d115a9cb977dd592a15bc4
Marian25 <marian.stanciulica@gmail.com>
2023-03-10 09:01:19 +0200

    Protocol 'LocalPhotoDataStore' added

---
+++ FoodybitePersistence/Cache/LocalPhotoDataStore.swift
@@ -0,0 +1,13 @@
+//
+//  LocalPhotoDataStore.swift
+//  FoodybitePersistence
+//
+//  Created by Marian Stanciulica on 10.03.2023.
+//
+
+import Foundation
+
+protocol LocalPhotoDataStore {
+    func read(photoReference: String) async throws -> Data?
+    func write(photoData: Data, for photoReference: String) async throws
+}
