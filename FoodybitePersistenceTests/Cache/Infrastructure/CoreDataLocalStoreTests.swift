f107f59e99188f0d7b52efd16e1e5555edab71dc
Marian25 <marian.stanciulica@gmail.com>
2023-03-14 20:18:31 +0200

    'CoreDataLocalStoreTests' update after 'LocalPhotoDataStore' was deleted

--- FoodybitePersistenceTests/Cache/Infrastructure/CoreDataLocalStoreTests.swift
+++ FoodybitePersistenceTests/Cache/Infrastructure/CoreDataLocalStoreTests.swift
@@ -11,8 +11,6 @@ import FoodybitePersistence
 
 final class CoreDataLocalStoreTests: XCTestCase {
     
-    // MARK: - LocalStore Tests
-    
     func test_read_deliversErrorOnCacheMiss() async {
         let sut = makeSUT()
 
@@ -79,46 +77,6 @@ final class CoreDataLocalStoreTests: XCTestCase {
         XCTAssertEqual(receivedUser, anyUser)
     }
     
-    // MARK: - LocalPhotoDataStore Tests
-    
-    func test_writeData_deliversErrorWhenPhotoNotFound() async throws {
-        let sut = makeSUT()
-        try await sut.write(Photo(width: 1, height: 1, photoReference: "correct reference"))
-
-        await expectWriteDataToFail(sut: sut, for: "incorrect reference")
-    }
-    
-    func test_readData_deliversErrorOnEmptyCache() async {
-        let sut = makeSUT()
-
-        await expectReadDataToFail(sut: sut, for: "reference")
-    }
-    
-    func test_readData_deliversErrorWhenPhotoNotFound() async throws {
-        let sut = makeSUT()
-        try await sut.write(Photo(width: 1, height: 1, photoReference: "correct reference"))
-        
-        await expectReadDataToFail(sut: sut, for: "incorrect reference")
-    }
-    
-    func test_readData_deliversErrorOnEmptyPhotoData() async throws {
-        let sut = makeSUT()
-        let correctReference = "correct reference"
-        try await sut.write(Photo(width: 1, height: 1, photoReference: correctReference))
-        
-        await expectReadDataToFail(sut: sut, for: correctReference)
-    }
-    
-    func test_readData_deliversPhotoDataOnCacheHit() async throws {
-        let sut = makeSUT()
-        let photoReference = "photo reference"
-        let expectedPhotoData = "photo data".data(using: .utf8)!
-        try await sut.write(Photo(width: 1, height: 1, photoReference: photoReference))
-        try await sut.save(photoData: expectedPhotoData, for: photoReference)
-
-        await expectReadDataToSucceed(sut: sut, for: photoReference, withExpected: expectedPhotoData)
-    }
-    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil) -> CoreDataLocalStore {
@@ -127,8 +85,6 @@ final class CoreDataLocalStoreTests: XCTestCase {
         return try! CoreDataLocalStore(storeURL: storeURL, bundle: storeBundle)
     }
 
-    // MARK: - LocalStore Helpers
-    
     private func expectReadToFail(sut: CoreDataLocalStore, file: StaticString = #file, line: UInt = #line) async {
         do {
             let _: User = try await sut.read()
@@ -163,35 +119,6 @@ final class CoreDataLocalStoreTests: XCTestCase {
     
     private func anotherUser(id: UUID = UUID()) -> User {
         return User(id: id, name: "another name", email: "another@email.com", profileImage: nil)
-    }
-    
-    // MARK: - LocalPhotoDataStore Helpers
-    
-    private func expectReadDataToFail(sut: CoreDataLocalStore, for photoReference: String, file: StaticString = #file, line: UInt = #line) async {
-        do {
-            let _: Data = try await sut.fetchPlacePhoto(photoReference: photoReference)
-            XCTFail("Read method expected to fail when cache miss", file: file, line: line)
-        } catch {
-            XCTAssertNotNil(error, file: file, line: line)
-        }
-    }
-    
-    private func expectReadDataToSucceed(sut: CoreDataLocalStore, for photoReference: String, withExpected expectedPhotoData: Data, file: StaticString = #file, line: UInt = #line) async {
-        do {
-            let receivedPhotoData: Data = try await sut.fetchPlacePhoto(photoReference: photoReference)
-            XCTAssertEqual(receivedPhotoData, expectedPhotoData, file: file, line: line)
-        } catch {
-            XCTFail("Expected to receive a resource, got \(error) instead", file: file, line: line)
-        }
-    }
-    
-    private func expectWriteDataToFail(sut: CoreDataLocalStore, for photoReference: String, file: StaticString = #file, line: UInt = #line) async {
-        do {
-            try await sut.save(photoData: "photo data".data(using: .utf8)!, for: photoReference)
-            XCTFail("Write method expected to fail when photo not found", file: file, line: line)
-        } catch {
-            XCTAssertNotNil(error, file: file, line: line)
-        }
     }
 
 }
