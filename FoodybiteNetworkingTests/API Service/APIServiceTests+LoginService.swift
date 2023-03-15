30ad495a654e54f4ce52ad47bc5d609c2a2f5344
Marian25 <marian.stanciulica@gmail.com>
2023-03-15 20:05:17 +0200

    APIServiceTests - update test for login to test URL components directly and remove the tests for the endpoint

--- FoodybiteNetworkingTests/API Service/APIServiceTests+LoginService.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests+LoginService.swift
@@ -8,6 +8,7 @@
 import XCTest
 @testable import FoodybiteNetworking
 import Domain
+import SharedAPI
 
 extension APIServiceTests {
     
@@ -16,31 +17,21 @@ extension APIServiceTests {
         XCTAssertNotNil(sut as LoginService)
     }
     
-    func test_login_loginParamsUsedToCreateEndpoint() async throws {
-        let email = anyEmail()
-        let password = anyPassword()
-        
-        let (sut, loader, _, _) = makeSUT(response: anyLoginResponse().response)
-        let loginEndpoint = LoginEndpoint.post(LoginRequest(email: email, password: password))
-        let urlRequest = try loginEndpoint.createURLRequest()
-        
-        _ = try await sut.login(email: email, password: password)
-        
-        let firstRequest = loader.requests.first
-        XCTAssertEqual(firstRequest?.httpBody, urlRequest.httpBody)
-    }
-    
     func test_login_usesLoginEndpointToCreateURLRequest() async throws {
         let email = anyEmail()
         let password = anyPassword()
         
         let (sut, loader, _, _) = makeSUT(response: anyLoginResponse().response)
-        let loginEndpoint = LoginEndpoint.post(LoginRequest(email: email, password: password))
-        let urlRequest = try loginEndpoint.createURLRequest()
+        let body = LoginRequest(email: email, password: password)
         
         _ = try await sut.login(email: email, password: password)
         
-        XCTAssertEqual(loader.requests, [urlRequest])
+        XCTAssertEqual(loader.requests.count, 1)
+        assertURLComponents(
+            urlRequest: loader.requests[0],
+            path: "/auth/login",
+            method: .post,
+            body: body)
     }
     
     func test_login_receiveExpectedLoginResponse() async throws {
@@ -62,6 +53,28 @@ extension APIServiceTests {
     }
     
     // MARK: - Helpers
+    
+    private func assertURLComponents(
+        urlRequest: URLRequest,
+        path: String,
+        method: RequestMethod,
+        body: Encodable,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let urlComponents = URLComponents(url: urlRequest.url!, resolvingAgainstBaseURL: true)
+
+        XCTAssertEqual(urlComponents?.scheme, "http", file: file, line: line)
+        XCTAssertEqual(urlComponents?.port, 8080, file: file, line: line)
+        XCTAssertEqual(urlComponents?.host, "localhost", file: file, line: line)
+        XCTAssertEqual(urlComponents?.path, path, file: file, line: line)
+        XCTAssertNil(urlComponents?.queryItems, file: file, line: line)
+        XCTAssertEqual(urlRequest.httpMethod, method.rawValue, file: file, line: line)
+        
+        let encoder = JSONEncoder()
+        let bodyData = try! encoder.encode(body)
+        XCTAssertEqual(urlRequest.httpBody, bodyData, file: file, line: line)
+    }
     
     private func anyLoginResponse() -> (response: LoginResponse, model: User) {
         let id = UUID()
