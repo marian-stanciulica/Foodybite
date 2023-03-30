30cfc30595f3314d7c9fd90df2fe24b38494ef64
Marian25 <marian.stanciulica@gmail.com>
2023-03-30 19:47:57 +0300

    CodableDataParser - remove unused JSONEncoder property

--- FoodybitePlaces/CodableDataParser.swift
+++ FoodybitePlaces/CodableDataParser.swift
@@ -9,14 +9,9 @@ import Foundation
 
 class CodableDataParser {
     private let jsonDecoder = JSONDecoder()
-    private let jsonEncoder = JSONEncoder()
     
     func decode<T: Decodable>(data: Data) throws -> T {
         jsonDecoder.dateDecodingStrategy = .iso8601
         return try jsonDecoder.decode(T.self, from: data)
-    }
-    
-    func encode<T: Encodable>(item: T) throws -> Data {
-        try jsonEncoder.encode(item)
     }
 }
