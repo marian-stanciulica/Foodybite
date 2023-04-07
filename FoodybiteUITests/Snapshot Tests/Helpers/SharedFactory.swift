4f7094f5b063dd16941a44b70b07167cf084b17f
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 16:18:26 +0300

    SharedFactory - updates multiline review text

--- FoodybiteUITests/Snapshot Tests/Helpers/SharedFactory.swift
+++ FoodybiteUITests/Snapshot Tests/Helpers/SharedFactory.swift
@@ -41,9 +41,9 @@ func makePhotoData() -> Data {
 
 func makeReviewText() -> String {
     """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet dapibus justo, eu cursus nulla. Nulla
-        viverra mollis ante et rutrum. Mauris lorem ante, congue eget malesuada quis, hendrerit vel elit. Suspendisse potenti.
-        Phasellus molestie vehicula blandit. Fusce sit amet egestas augue. Integer quis lacinia massa. Aliquam hendrerit arcu
-        eget leo congue maximus. Etiam interdum eget mi at consectetur.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet dapibus justo, eu cursus nulla. Nulla \
+    viverra mollis ante et rutrum. Mauris lorem ante, congue eget malesuada quis, hendrerit vel elit. Suspendisse potenti. \
+    Phasellus molestie vehicula blandit. Fusce sit amet egestas augue. Integer quis lacinia massa. Aliquam hendrerit arcu \
+    eget leo congue maximus. Etiam interdum eget mi at consectetur.
     """
 }
