a84ae48349fddba57ff2c07a04a47addf5f9c4e4
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:46:53 +0300

    Makes 'APIServiceTests+ChangePasswordService' conform to the SwiftLint style conventions

--- FoodybiteNetworkingTests/API Service/APIServiceTests+ChangePasswordService.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests+ChangePasswordService.swift
@@ -10,30 +10,30 @@ import XCTest
 import Domain
 
 extension APIServiceTests {
-    
+
     func test_conformsToChangePasswordService() {
         let (sut, _, _, _) = makeSUT()
         XCTAssertNotNil(sut as ChangePasswordService)
     }
-    
+
     func test_changePassword_usesChangePasswordEndpointToCreateURLRequest() async throws {
         let (sut, _, sender, _) = makeSUT()
-        
+
         let currentPassword = anyPassword()
         let hashedCurrentPassword = hash(password: currentPassword)
-        
+
         let newPassword = anyPassword()
         let hashedNewPassword = hash(password: newPassword)
-        
+
         let confirmPassword = anyPassword()
         let hashedConfirmPassword = hash(password: confirmPassword)
-        
+
         try await sut.changePassword(
             currentPassword: currentPassword,
             newPassword: newPassword,
             confirmPassword: confirmPassword
         )
-        
+
         XCTAssertEqual(sender.requests.count, 1)
         assertURLComponents(
             urlRequest: sender.requests[0],
@@ -46,9 +46,9 @@ extension APIServiceTests {
             )
         )
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeChangePasswordRequestBody(currentPassword: String, newPassword: String, confirmPassword: String) -> ChangePasswordRequestBody {
         ChangePasswordRequestBody(
             currentPassword: currentPassword,
