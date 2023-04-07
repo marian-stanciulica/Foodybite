77a835bf59bc56983658ef1bb1f10c7ba605da00
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:13:27 +0300

    Makes 'PhoneAndDirectionsView' conform to the SwiftLint style conventions

--- FoodybiteUI/Views/Restaurant Details/Restaurant Image/PhoneAndDirectionsView.swift
+++ FoodybiteUI/Views/Restaurant Details/Restaurant Image/PhoneAndDirectionsView.swift
@@ -10,38 +10,38 @@ import SwiftUI
 struct PhoneAndDirectionsView: View {
     let phoneNumber: String?
     let showMaps: () -> Void
-    
+
     var body: some View {
         HStack {
             if let phoneNumber = phoneNumber {
                 Spacer()
-                
+
                 Image(systemName: "star")
                     .resizable()
                     .aspectRatio(1, contentMode: .fit)
                     .foregroundColor(.white)
                     .frame(height: 24)
-                
-                
+
+
                 Text(phoneNumber)
                     .font(.callout)
                     .foregroundColor(.white)
-            
+
                 Spacer()
                 Rectangle()
                     .frame(width: 1)
                     .foregroundColor(.white.opacity(0.25))
             }
-            
+
             Spacer()
-            
+
             Group {
                 Image(systemName: "arrow.uturn.right.circle")
                     .resizable()
                     .aspectRatio(1, contentMode: .fit)
                     .foregroundColor(.white)
                     .frame(height: 24)
-                
+
                 Text("Direction")
                     .font(.callout)
                     .foregroundColor(.white)
