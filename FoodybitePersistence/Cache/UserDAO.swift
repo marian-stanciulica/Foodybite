d1277213e2309a4c22c8fb12903535e27b6988e8
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 16:04:33 +0300

    Makes 'UserDAO' conform to the SwiftLint style conventions

--- FoodybitePersistence/Cache/UserDAO.swift
+++ FoodybitePersistence/Cache/UserDAO.swift
@@ -10,20 +10,20 @@ import Domain
 
 public final class UserDAO {
     private let store: LocalStore
-    
+
     private struct UserNotFound: Error {}
-    
+
     public init(store: LocalStore) {
         self.store = store
     }
-    
+
     public func getUser(id: UUID) async throws -> User {
         let users: [User] = try await store.readAll()
-        
+
         guard let foundUser = users.first(where: { $0.id == id }) else {
             throw UserNotFound()
         }
-        
+
         return foundUser
     }
 }
