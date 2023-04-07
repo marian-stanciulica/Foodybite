89c8d16a5edf06fd48c6e312de22759234fd7be9
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:54:22 +0300

    Makes 'SearchCriteriaViewSnapshotTests' conform to the SwiftLint style conventions

--- FoodybiteUITests/Snapshot Tests/Search Filtering/SearchCriteriaViewSnapshotTests.swift
+++ FoodybiteUITests/Snapshot Tests/Search Filtering/SearchCriteriaViewSnapshotTests.swift
@@ -12,16 +12,16 @@ import FoodybitePresentation
 import FoodybiteUI
 
 final class SearchCriteriaViewSnapshotTests: XCTestCase {
-    
+
     func test_searchCriteriaView() {
         let sut = makeSUT(radius: 20, starsNumber: 4)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT(radius: CGFloat, starsNumber: Int) -> SearchCriteriaView {
         let viewModel = SearchCriteriaViewModel(
             userPreferences: makeUserPreferences(),
@@ -29,13 +29,12 @@ final class SearchCriteriaViewSnapshotTests: XCTestCase {
         )
         return SearchCriteriaView(viewModel: viewModel)
     }
-    
+
     private func makeUserPreferences() -> UserPreferences {
         UserPreferences(radius: 10_000, starsNumber: 4)
     }
-    
+
     private class EmptyUserPreferencesSaver: UserPreferencesSaver {
         func save(_ userPreferences: UserPreferences) {}
     }
 }
- 
