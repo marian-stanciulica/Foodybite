4f7e5fadaf9d14b41d6151791422a21b68f6e378
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:58:12 +0300

    Makes 'LoginViewSnapshotTests' conform to the SwiftLint style conventions

--- FoodybiteUITests/Snapshot Tests/Login/LoginViewSnapshotTests.swift
+++ FoodybiteUITests/Snapshot Tests/Login/LoginViewSnapshotTests.swift
@@ -12,32 +12,32 @@ import FoodybitePresentation
 import FoodybiteUI
 
 final class LoginViewSnapshotTests: XCTestCase {
- 
+
     func test_loginViewIdleState() {
         let sut = makeSUT(state: .idle)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_loginViewIsLoadingState() {
         let sut = makeSUT(email: "testing@testing.com",
                           password: "12345678",
                           state: .isLoading)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_loginViewFailureState() {
         let sut = makeSUT(state: .failure(.serverError))
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT(email: String = "", password: String = "", state: LoginViewModel.State) -> LoginView {
         let viewModel = LoginViewModel(loginService: EmptyLoginService(), goToMainTab: { _ in })
         viewModel.email = email
@@ -45,7 +45,7 @@ final class LoginViewSnapshotTests: XCTestCase {
         viewModel.state = state
         return LoginView(viewModel: viewModel, goToSignUp: {})
     }
-    
+
     private class EmptyLoginService: LoginService {
         func login(email: String, password: String) async throws -> User {
             User(id: UUID(), name: "", email: "", profileImage: nil)
