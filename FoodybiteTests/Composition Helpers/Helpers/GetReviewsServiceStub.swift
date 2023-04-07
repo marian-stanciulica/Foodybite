60a4c2fe629471ee804e3e48dbe687fea7cbe4df
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:44:03 +0300

    Makes 'GetReviewsServiceStub' conform to the SwiftLint style conventions

--- FoodybiteTests/Composition Helpers/Helpers/GetReviewsServiceStub.swift
+++ FoodybiteTests/Composition Helpers/Helpers/GetReviewsServiceStub.swift
@@ -11,14 +11,14 @@ import Domain
 final class GetReviewsServiceStub: GetReviewsService {
     private(set) var capturedValues = [String?]()
     var stub: Result<[Review], Error>?
-    
+
     func getReviews(restaurantID: String?) async throws -> [Review] {
         capturedValues.append(restaurantID)
-        
+
         if let stub = stub {
             return try stub.get()
         }
-        
+
         return []
     }
 }
