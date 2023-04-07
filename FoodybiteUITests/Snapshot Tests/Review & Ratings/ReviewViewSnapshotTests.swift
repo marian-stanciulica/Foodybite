0401e292894b06893340a335d636c23310732cfa
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:52:32 +0300

    Makes 'ReviewViewSnapshotTests' conform to the SwiftLint style conventions

--- FoodybiteUITests/Snapshot Tests/Review & Ratings/ReviewViewSnapshotTests.swift
+++ FoodybiteUITests/Snapshot Tests/Review & Ratings/ReviewViewSnapshotTests.swift
@@ -12,34 +12,34 @@ import FoodybitePresentation
 import FoodybiteUI
 
 final class ReviewViewSnapshotTests: XCTestCase {
-    
+
     func test_reviewViewIdleState() {
         let sut = makeSUT(state: .idle)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_reviewViewIsLoadingState() {
         let sut = makeSUT(starsNumber: 4,
                           reviewText: makeReviewText(),
                           state: .isLoading)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_reviewViewFailureState() {
         let sut = makeSUT(starsNumber: 4,
                           reviewText: makeReviewText(),
                           state: .failure(.serverError))
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT(starsNumber: Int = 0, reviewText: String = "", state: ReviewViewModel.State) -> ReviewView {
         let viewModel = ReviewViewModel(restaurantID: "", reviewService: EmptyAddReviewService())
         viewModel.starsNumber = starsNumber
@@ -47,7 +47,7 @@ final class ReviewViewSnapshotTests: XCTestCase {
         viewModel.state = state
         return ReviewView(viewModel: viewModel, dismissScreen: {})
     }
-    
+
     private class EmptyAddReviewService: AddReviewService {
         func addReview(restaurantID: String, reviewText: String, starsNumber: Int, createdAt: Date) async throws {}
     }
