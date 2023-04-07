a5fd7b6829dee785cbc0d0eb3368cf2caaa7d0f9
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:57:16 +0300

    Makes 'SettingsViewSnapshotTests' conform to the SwiftLint style conventions

--- FoodybiteUITests/Snapshot Tests/Profile/Settings/Settings/SettingsViewSnapshotTests.swift
+++ FoodybiteUITests/Snapshot Tests/Profile/Settings/Settings/SettingsViewSnapshotTests.swift
@@ -12,21 +12,21 @@ import FoodybitePresentation
 import FoodybiteUI
 
 final class SettingsViewSnapshotTests: XCTestCase {
-    
+
     func test_settingsViewIdleState() {
         let sut = makeSUT()
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> SettingsView {
         let viewModel = SettingsViewModel(logoutService: EmptyLogoutService(), goToLogin: {})
         return SettingsView(viewModel: viewModel, goToChangePassword: {})
     }
-    
+
     private class EmptyLogoutService: LogoutService {
         func logout() async throws {}
     }
