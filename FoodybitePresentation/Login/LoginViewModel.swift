7efdbeaea7809124b42090a77b223568e0597b91
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:57:53 +0300

    Makes 'LoginViewModel' conform to the SwiftLint style conventions

--- FoodybitePresentation/Login/LoginViewModel.swift
+++ FoodybitePresentation/Login/LoginViewModel.swift
@@ -12,33 +12,33 @@ final public class LoginViewModel: ObservableObject {
     public enum LoginError: String, Error {
         case serverError = "Invalid Credentials"
     }
-    
+
     public enum State: Equatable {
         case idle
         case isLoading
         case success
         case failure(LoginError)
     }
-    
+
     private let loginService: LoginService
     private let goToMainTab: (User) -> Void
-    
+
     @Published public var email = ""
     @Published public var password = ""
     @Published public var state: State = .idle
-    
+
     public var isLoading: Bool {
         state == .isLoading
     }
-    
+
     public init(loginService: LoginService, goToMainTab: @escaping (User) -> Void) {
         self.loginService = loginService
         self.goToMainTab = goToMainTab
     }
-    
+
     @MainActor public func login() async {
         state = .isLoading
-        
+
         do {
             let user = try await loginService.login(email: email, password: password)
             goToMainTab(user)
@@ -46,5 +46,5 @@ final public class LoginViewModel: ObservableObject {
             state = .failure(.serverError)
         }
     }
-    
+
 }
