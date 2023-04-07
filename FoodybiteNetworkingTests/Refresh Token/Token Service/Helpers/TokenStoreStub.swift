095bd1d61a1dea8dc7b48bdd8524acda4b02689a
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 16:01:17 +0300

    Makes 'TokenStoreStub' conform to the SwiftLint style conventions

--- FoodybiteNetworkingTests/Refresh Token/Token Service/Helpers/TokenStoreStub.swift
+++ FoodybiteNetworkingTests/Refresh Token/Token Service/Helpers/TokenStoreStub.swift
@@ -11,26 +11,26 @@ import FoodybiteNetworking
 class TokenStoreStub: TokenStore {
     private var stub: AuthToken?
     private(set) var writeCount = 0
-    
+
     init() {}
-    
+
     init(_ initialValue: AuthToken) {
         stub = initialValue
     }
-    
+
     func read() throws -> AuthToken {
         guard let stub = stub else {
             throw NSError(domain: "any error", code: 1)
         }
-        
+
         return stub
     }
-    
+
     func write(_ token: AuthToken) throws {
         writeCount += 1
         stub = token
     }
-    
+
     func delete() throws {
         stub = nil
     }
