9005006552bf6344f6da07cafba7ffee2c200c91
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:17:53 +0300

    Makes 'PlacesServiceTests+AutocompletePlacesService' conform to the SwiftLint style conventions

--- FoodybitePlacesTests/Place Service/PlacesServiceTests+AutocompletePlacesService.swift
+++ FoodybitePlacesTests/Place Service/PlacesServiceTests+AutocompletePlacesService.swift
@@ -10,22 +10,22 @@ import XCTest
 import Domain
 
 extension PlacesServiceTests {
-    
+
     func test_conformsToAutocompleteRestaurantsService() {
         let (sut, _) = makeSUT(response: anyData())
         XCTAssertNotNil(sut as AutocompleteRestaurantsService)
     }
-    
+
     func test_autocomplete_usesAutocompleteEndpointToCreateURLRequest() async throws {
         let input = "input"
         let location = Location(latitude: -33.8, longitude: 15.1)
         let radius = 15
-        
+
         let (sut, loader) = makeSUT(response: anyAutocompleteResponse())
         let endpoint = AutocompleteEndpoint(input: input, location: location, radius: radius)
-        
+
         _ = try await sut.autocomplete(input: input, location: location, radius: radius)
-        
+
         XCTAssertEqual(loader.getRequests.count, 1)
         assertURLComponents(
             urlRequest: loader.getRequests[0],
@@ -35,11 +35,11 @@ extension PlacesServiceTests {
             apiKey: endpoint.apiKey
         )
     }
-    
+
     func test_autocomplete_throwsErrorWhenStatusIsNotOK() async {
         let autocompleteResponse = anyAutocompleteResponse(status: .overQueryLimit)
         let (sut, _) = makeSUT(response: autocompleteResponse)
-        
+
         do {
             let autocompletePredictions = try await autocomplete(on: sut)
             XCTFail("Expected to fail, got \(autocompletePredictions) instead")
@@ -47,18 +47,18 @@ extension PlacesServiceTests {
             XCTAssertNotNil(error)
         }
     }
-    
+
     func test_autocomplete_receiveExpectedAutocompleteResponse() async throws {
         let expectedAutocompleteResponse = anyAutocompleteResponse()
         let (sut, _) = makeSUT(response: expectedAutocompleteResponse)
-        
+
         let receivedAutocompletePredictions = try await autocomplete(on: sut)
-        
+
         XCTAssertEqual(expectedAutocompleteResponse.autocompletePredictions, receivedAutocompletePredictions)
     }
-    
+
     // MARK: - Helpers
-    
+
     private func assertURLComponents(
         urlRequest: URLRequest,
         input: String,
@@ -75,7 +75,7 @@ extension PlacesServiceTests {
             URLQueryItem(name: "radius", value: "\(radius)"),
             URLQueryItem(name: "type", value: "restaurant")
         ]
-        
+
         assertURLComponents(
             urlRequest: urlRequest,
             path: "/maps/api/place/autocomplete/json",
@@ -83,27 +83,56 @@ extension PlacesServiceTests {
             file: file,
             line: line)
     }
-    
-    private func autocomplete(on sut: PlacesService, input: String? = nil, location: Location? = nil, radius: Int? = nil) async throws -> [AutocompletePrediction] {
+
+    private func autocomplete(
+        on sut: PlacesService,
+        input: String? = nil,
+        location: Location? = nil,
+        radius: Int? = nil
+    ) async throws -> [AutocompletePrediction] {
         let defaultInput = "input"
         let defaultLocation = Location(latitude: -33.8, longitude: 15.1)
         let defaultRadius = 15
-        
+
         return try await sut.autocomplete(
             input: input ?? defaultInput,
             location: location ?? defaultLocation,
             radius: radius ?? defaultRadius
         )
     }
-    
+
     private func anyAutocompleteResponse(status: AutocompleteStatus = .ok) -> AutocompleteResponse {
         AutocompleteResponse(
             predictions: [
-                Prediction(description: "a description", matchedSubstrings: [], placeID: "place #1", reference: "", structuredFormatting: StructuredFormatting(mainText: "", mainTextMatchedSubstrings: [], secondaryText: ""), terms: [], types: []),
-                Prediction(description: "another description", matchedSubstrings: [], placeID: "place #2", reference: "", structuredFormatting: StructuredFormatting(mainText: "", mainTextMatchedSubstrings: [], secondaryText: ""), terms: [], types: [])
+                Prediction(
+                    description: "a description",
+                    matchedSubstrings: [],
+                    placeID: "place #1",
+                    reference: "",
+                    structuredFormatting: StructuredFormatting(
+                        mainText: "",
+                        mainTextMatchedSubstrings: [],
+                        secondaryText: ""
+                    ),
+                    terms: [],
+                    types: []
+                ),
+                Prediction(
+                    description: "another description",
+                    matchedSubstrings: [],
+                    placeID: "place #2",
+                    reference: "",
+                    structuredFormatting: StructuredFormatting(
+                        mainText: "",
+                        mainTextMatchedSubstrings: [],
+                        secondaryText: ""
+                    ),
+                    terms: [],
+                    types: []
+                )
             ],
             status: status
         )
     }
-    
+
 }
