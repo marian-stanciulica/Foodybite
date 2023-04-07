28f35a2a9a8bb8c12ff8ada8cb8afa3410e0ccb2
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:47:23 +0300

    Makes 'APIServiceTests+LoginService' conform to the SwiftLint style conventions

--- FoodybiteNetworkingTests/API Service/APIServiceTests+LoginService.swift
+++ FoodybiteNetworkingTests/API Service/APIServiceTests+LoginService.swift
@@ -10,20 +10,20 @@ import XCTest
 import Domain
 
 extension APIServiceTests {
-    
+
     func test_conformsToLoginService() {
         let (sut, _, _, _) = makeSUT()
         XCTAssertNotNil(sut as LoginService)
     }
-    
+
     func test_login_usesLoginEndpointToCreateURLRequest() async throws {
         let email = anyEmail()
         let password = anyPassword()
         let hashedPassword = hash(password: password)
         let (sut, loader, _, _) = makeSUT(response: anyLoginResponse())
-        
+
         _ = try await sut.login(email: email, password: password)
-        
+
         XCTAssertEqual(loader.requests.count, 1)
         assertURLComponents(
             urlRequest: loader.requests[0],
@@ -31,27 +31,27 @@ extension APIServiceTests {
             method: .post,
             body: LoginRequestBody(email: email, password: hashedPassword))
     }
-    
+
     func test_login_receiveExpectedLoginResponse() async throws {
         let expectedModel = anyLoginResponse()
         let (sut, _, _, _) = makeSUT(response: expectedModel)
-        
+
         let receivedUser = try await sut.login(email: anyEmail(), password: anyPassword())
-        
+
         XCTAssertEqual(expectedModel.remoteUser.model, receivedUser)
     }
-    
+
     func test_login_storesAuthTokenInKeychain() async throws {
         let (sut, _, _, tokenStoreStub) = makeSUT(response: anyLoginResponse())
-        
+
         _ = try await sut.login(email: anyEmail(), password: anyPassword())
         let receivedToken = try tokenStoreStub.read()
-        
+
         XCTAssertEqual(receivedToken, anyAuthToken())
     }
-    
+
     // MARK: - Helpers
-    
+
     private func anyLoginResponse() -> LoginResponse {
         LoginResponse(
             remoteUser: RemoteUser(
@@ -63,7 +63,7 @@ extension APIServiceTests {
             token: anyAuthToken()
         )
     }
-    
+
     private func anyAuthToken() -> AuthToken {
         AuthToken(accessToken: "any access token",
                          refreshToken: "any refresh token")
