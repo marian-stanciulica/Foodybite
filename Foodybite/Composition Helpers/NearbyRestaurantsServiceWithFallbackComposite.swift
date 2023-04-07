cf2edb7b9d766711090456b64c41b043730d11ba
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:05:55 +0300

    Makes 'NearbyRestaurantsServiceWithFallbackComposite' conform to the SwiftLint style conventions

--- Foodybite/Composition Helpers/NearbyRestaurantsServiceWithFallbackComposite.swift
+++ Foodybite/Composition Helpers/NearbyRestaurantsServiceWithFallbackComposite.swift
@@ -10,12 +10,12 @@ import Domain
 public final class NearbyRestaurantsServiceWithFallbackComposite: NearbyRestaurantsService {
     private let primary: NearbyRestaurantsService
     private let secondary: NearbyRestaurantsService
-    
+
     public init(primary: NearbyRestaurantsService, secondary: NearbyRestaurantsService) {
         self.primary = primary
         self.secondary = secondary
     }
-    
+
     public func searchNearby(location: Location, radius: Int) async throws -> [NearbyRestaurant] {
         do {
             return try await primary.searchNearby(location: location, radius: radius)
