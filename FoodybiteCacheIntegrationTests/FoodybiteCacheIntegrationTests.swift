8c250dda40a97762376cbfa88a0adb7864c7e49a
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 12:53:07 +0300

    Makes 'FoodybiteUserCacheIntegrationTests' conform to the SwiftLint style conventions

--- FoodybiteCacheIntegrationTests/FoodybiteCacheIntegrationTests.swift
+++ FoodybiteCacheIntegrationTests/FoodybiteCacheIntegrationTests.swift
@@ -13,80 +13,78 @@ final class FoodybiteUserCacheIntegrationTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
+
         setupEmptyStoreState()
     }
-    
+
     override func tearDown() {
         super.tearDown()
-        
+
         undoStoreSideEffects()
     }
-    
-    // MARK: - LocalUserLoader Tests
-    
-    func test_loadUser_deliversNilOnEmptyCache() async {
-        let userLoader = makeUserLoader()
-        
-        let user: User? = try? await userLoader.read()
-        
+
+    func test_loadUser_deliversNilOnEmptyCache() async throws {
+        let localStore = try makeLocalStore()
+
+        let user: User? = try? await localStore.read()
+
         XCTAssertEqual(user, nil)
     }
-    
+
     func test_loadUser_deliversUserSavedOnASeparateInstance() async throws {
-        let userLoaderToPerformSave = makeUserLoader()
-        let userLoaderToPerformLoad = makeUserLoader()
+        let localStoreToPerformSave = try makeLocalStore()
+        let localStoreToPerformLoad = try makeLocalStore()
         let user = makeUser()
-        
-        try await userLoaderToPerformSave.write(user)
-        
-        let receivedUser: User = try await userLoaderToPerformLoad.read()
+
+        try await localStoreToPerformSave.write(user)
+
+        let receivedUser: User = try await localStoreToPerformLoad.read()
         XCTAssertEqual(receivedUser, user)
     }
-    
+
     func test_saveUser_updatesUserSavedOnASeparateInstance() async throws {
-        let userLoaderToPerformFirstSave = makeUserLoader()
-        let userLoaderToPerformSecondSave = makeUserLoader()
-        let userLoaderToPerformLoad = makeUserLoader()
+        let localStoreToPerformFirstSave = try makeLocalStore()
+        let localStoreToPerformSecondSave = try makeLocalStore()
+        let localStoreToPerformLoad = try makeLocalStore()
         let userID = UUID()
         let firstUser = makeUser(id: userID)
         let secondUser = makeUser(id: userID)
-        
-        try await userLoaderToPerformFirstSave.write(firstUser)
-        try await userLoaderToPerformSecondSave.write(secondUser)
-        
-        let receivedUser: User = try await userLoaderToPerformLoad.read()
+
+        try await localStoreToPerformFirstSave.write(firstUser)
+        try await localStoreToPerformSecondSave.write(secondUser)
+
+        let receivedUser: User = try await localStoreToPerformLoad.read()
         XCTAssertEqual(receivedUser, secondUser)
     }
-    
+
     // MARK: - Helpers
-    
-    private func makeUserLoader() -> LocalStore {
+
+    private func makeLocalStore() throws -> LocalStore {
         let storeURL = testSpecificStoreURL()
-        let sut = try! CoreDataLocalStore(storeURL: storeURL)
+        let sut = try CoreDataLocalStore(storeURL: storeURL)
         return sut
     }
-    
+
     private func makeUser(id: UUID = UUID()) -> User {
         User(id: id, name: "User #1", email: "testing@testing.com", profileImage: nil)
     }
-    
+
     private func testSpecificStoreURL() -> URL {
         return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }
-    
+
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
-    
+
     private func setupEmptyStoreState() {
         deleteStoreArtifacts()
     }
-    
+
     private func undoStoreSideEffects() {
         deleteStoreArtifacts()
     }
-    
+
     private func deleteStoreArtifacts() {
         try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
