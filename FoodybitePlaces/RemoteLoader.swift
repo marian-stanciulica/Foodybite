5f52c13a1e31a339f791fed4834a7fe5300a4219
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:23:39 +0300

    Makes 'RemoteLoader' conform to the SwiftLint style conventions

--- FoodybitePlaces/RemoteLoader.swift
+++ FoodybitePlaces/RemoteLoader.swift
@@ -10,36 +10,36 @@ import Foundation
 public class RemoteLoader: ResourceLoader, DataLoader {
     private let client: HTTPClient
     private let codableDataParser = CodableDataParser()
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    
+
     public init(client: HTTPClient) {
         self.client = client
     }
-    
+
     public func get<T: Decodable>(for urlRequest: URLRequest) async throws -> T {
         let data = try await getData(for: urlRequest)
-        
+
         guard let decodable: T = try? codableDataParser.decode(data: data) else {
             throw Error.invalidData
         }
-        
+
         return decodable
     }
-    
+
     public func getData(for urlRequest: URLRequest) async throws -> Data {
         guard let result = try? await client.send(urlRequest) else {
             throw Error.connectivity
         }
-        
+
         guard (200..<300).contains(result.response.statusCode) else {
             throw Error.invalidData
         }
-        
+
         return result.data
     }
-    
+
 }
