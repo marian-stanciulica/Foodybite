7fcdb0c86649d5a0af3e1d89d733fa0254226e72
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:25:15 +0300

    Makes 'RestaurantCellViewModel' conform to the SwiftLint style conventions

--- FoodybitePresentation/Home/RestaurantCellViewModel.swift
+++ FoodybitePresentation/Home/RestaurantCellViewModel.swift
@@ -11,24 +11,24 @@ import Domain
 public final class RestaurantCellViewModel: ObservableObject {
     private let nearbyRestaurant: NearbyRestaurant
     private let distanceInKmFromCurrentLocation: Double
-    
+
     public init(nearbyRestaurant: NearbyRestaurant, distanceInKmFromCurrentLocation: Double) {
         self.nearbyRestaurant = nearbyRestaurant
         self.distanceInKmFromCurrentLocation = distanceInKmFromCurrentLocation
     }
-    
+
     public var isOpen: Bool {
         nearbyRestaurant.isOpen
     }
-    
+
     public var restaurantName: String {
         nearbyRestaurant.name
     }
-    
+
     public var rating: String {
         String(format: "%.1f", nearbyRestaurant.rating)
     }
-    
+
     public var distance: String {
         return "\(distanceInKmFromCurrentLocation)"
     }
