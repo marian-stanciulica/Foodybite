c3d7f6dc6123763adbd31accd75fa315cf190b6d
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:21:25 +0300

    Makes 'RefreshTokenService' conform to the SwiftLint style conventions

--- FoodybiteNetworking/Refresh Token/Token Service/RefreshTokenService.swift
+++ FoodybiteNetworking/Refresh Token/Token Service/RefreshTokenService.swift
@@ -11,34 +11,34 @@ public actor RefreshTokenService: TokenRefresher {
     private let loader: ResourceLoader
     private let tokenStore: TokenStore
     private var refreshTask: Task<Void, Error>?
-    
+
     public init(loader: ResourceLoader, tokenStore: TokenStore) {
         self.loader = loader
         self.tokenStore = tokenStore
     }
-    
+
     nonisolated public func getLocalToken() throws -> AuthToken {
         return try tokenStore.read()
     }
-    
+
     public func fetchLocallyRemoteToken() async throws {
         if let refreshTask = refreshTask {
             return try await refreshTask.value
         }
-        
+
         let urlRequest = try createURLRequest()
-        
+
         let task: Task<Void, Error> = Task {
             let remoteAuthToken: AuthToken = try await loader.get(for: urlRequest)
             try tokenStore.write(remoteAuthToken)
-            
+
             refreshTask = nil
         }
-        
+
         refreshTask = task
         try await task.value
     }
-    
+
     private func createURLRequest() throws -> URLRequest {
         let authToken = try tokenStore.read()
         let body = RefreshTokenRequestBody(refreshToken: authToken.refreshToken)
