c0e3fd22f57a51a15d6a04ac1f0ec430bceb4845
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:22:10 +0300

    Makes 'RegisterValidatorTests' conform to the SwiftLint style conventions

--- FoodybitePresentationTests/Register/RegisterValidatorTests.swift
+++ FoodybitePresentationTests/Register/RegisterValidatorTests.swift
@@ -9,21 +9,21 @@ import XCTest
 import FoodybitePresentation
 
 final class RegisterValidatorTests: XCTestCase {
-    
+
     func test_registrationError_rawValueOfServerError() {
         XCTAssertEqual(RegisterValidator.Error.serverError.toString(), "Server error")
     }
-    
+
     func test_registrationError_rawValueOfEmptyNameError() {
         XCTAssertEqual(RegisterValidator.Error.emptyName.toString(), "Empty name")
     }
-    
+
     func test_registrationError_rawValueOfEmptyEmailError() {
         XCTAssertEqual(RegisterValidator.Error.emptyEmail.toString(), "Empty email")
     }
-    
+
     func test_registrationError_rawValueOfInvalidEmailError() {
         XCTAssertEqual(RegisterValidator.Error.invalidEmail.toString(), "Invalid email")
     }
-    
+
 }
