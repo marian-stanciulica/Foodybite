7c2eee2676a706e68478beed668eb57210e605b0
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 16:04:55 +0300

    Makes 'UserDAOTests' conform to the SwiftLint style conventions

--- FoodybitePersistenceTests/Cache/UserDAOTests.swift
+++ FoodybitePersistenceTests/Cache/UserDAOTests.swift
@@ -10,11 +10,11 @@ import Domain
 import FoodybitePersistence
 
 final class UserDAOTests: XCTestCase {
-    
+
     func test_getUser_throwsErrorWhenStoreThrowsError() async {
         let (sut, storeSpy) = makeSUT()
         storeSpy.readResult = .failure(anyError())
-        
+
         do {
             let user = try await sut.getUser(id: UUID())
             XCTFail("Expected to fail, received user \(user) instead")
@@ -22,11 +22,11 @@ final class UserDAOTests: XCTestCase {
             XCTAssertNotNil(error)
         }
     }
-    
+
     func test_getUser_throwsErrorWhenUserNotFound() async {
         let (sut, storeSpy) = makeSUT()
         storeSpy.readAllResult = .success([makeUser(), makeUser(), makeUser()])
-        
+
         do {
             let user = try await sut.getUser(id: UUID())
             XCTFail("Expected to fail, received user \(user) instead")
@@ -34,26 +34,26 @@ final class UserDAOTests: XCTestCase {
             XCTAssertNotNil(error)
         }
     }
-    
+
     func test_getUser_returnsFoundUserWhenFoundInStore() async throws {
         let (sut, storeSpy) = makeSUT()
         let expectedUser = makeUser()
         storeSpy.readAllResult = .success([makeUser(), makeUser()] + [expectedUser])
-        
+
         let receivedUser = try await sut.getUser(id: expectedUser.id)
         XCTAssertEqual(expectedUser, receivedUser)
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> (sut: UserDAO, storeSpy: LocalStoreSpy) {
         let storeSpy = LocalStoreSpy()
         let sut = UserDAO(store: storeSpy)
         return (sut, storeSpy)
     }
-    
+
     private func makeUser() -> User {
         User(id: UUID(), name: "User", email: "user@user.com", profileImage: nil)
     }
-    
+
 }
