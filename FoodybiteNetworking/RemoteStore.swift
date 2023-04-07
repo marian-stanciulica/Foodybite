35de1272b2230b50ed0233490c49f6550f8fa5a2
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:24:29 +0300

    Makes 'RemoteStore' conform to the SwiftLint style conventions

--- FoodybiteNetworking/RemoteStore.swift
+++ FoodybiteNetworking/RemoteStore.swift
@@ -10,38 +10,38 @@ import Foundation
 public class RemoteStore: ResourceLoader, ResourceSender {
     private let client: HTTPClient
     private let codableDataParser = CodableDataParser()
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidRequest
         case invalidData
     }
-    
+
     public init(client: HTTPClient) {
         self.client = client
     }
-    
+
     public func get<T: Decodable>(for urlRequest: URLRequest) async throws -> T {
         guard let result = try? await client.send(urlRequest) else {
             throw Error.connectivity
         }
-        
+
         guard (200..<300).contains(result.response.statusCode) else {
             throw Error.invalidRequest
         }
-        
+
         guard let decodable: T = try? codableDataParser.decode(data: result.data) else {
             throw Error.invalidData
         }
-        
+
         return decodable
     }
-    
+
     public func post(to urlRequest: URLRequest) async throws {
         guard let result = try? await client.send(urlRequest) else {
             throw Error.connectivity
         }
-        
+
         guard (200..<300).contains(result.response.statusCode) else {
             throw Error.invalidRequest
         }
