37e8108177b45146a8c6b93e91fe2a517692cff0
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:51:09 +0300

    Makes 'HomeView' conform to the SwiftLint style conventions

--- FoodybiteUI/Views/Home/HomeView.swift
+++ FoodybiteUI/Views/Home/HomeView.swift
@@ -14,7 +14,7 @@ public struct HomeView<Cell: View, SearchView: View>: View {
     let showRestaurantDetails: (String) -> Void
     let cell: (NearbyRestaurant) -> Cell
     let searchView: (Binding<String>) -> SearchView
-    
+
     public init(
         viewModel: HomeViewModel,
         showRestaurantDetails: @escaping (String) -> Void,
@@ -26,27 +26,27 @@ public struct HomeView<Cell: View, SearchView: View>: View {
         self.cell = cell
         self.searchView = searchView
     }
-    
+
     public var body: some View {
         VStack {
             switch viewModel.searchNearbyState {
             case .idle:
                 EmptyView()
-                
+
             case .isLoading:
                 ProgressView()
                 Spacer()
-                
+
             case let .failure(error):
                 Text(error.rawValue)
                     .foregroundColor(.red)
                 Spacer()
-                
+
             case .success:
                 ScrollView(.vertical, showsIndicators: false) {
                     searchView($viewModel.searchText)
                         .padding(.bottom)
-                    
+
                     LazyVStack {
                         ForEach(viewModel.filteredNearbyRestaurants, id: \.id) { restaurant in
                             cell(restaurant)
@@ -67,7 +67,7 @@ public struct HomeView<Cell: View, SearchView: View>: View {
         .padding(.horizontal)
         .task {
             guard viewModel.searchNearbyState == .idle else { return }
-            
+
             await viewModel.searchNearby()
         }
     }
@@ -98,17 +98,38 @@ struct HomeView_Previews: PreviewProvider {
                  searchView: { _ in EmptyView() }
         )
     }
-    
+
     private class PreviewNearbyRestaurantsService: NearbyRestaurantsService {
         func searchNearby(location: Location, radius: Int) async throws -> [NearbyRestaurant] {
             [
-                NearbyRestaurant(id: "#1", name: "Place name #1", isOpen: true, rating: 3.4, location: Location(latitude: 0, longitude: 0), photo: nil),
-                NearbyRestaurant(id: "#2", name: "Place name #2", isOpen: false, rating: 4.3, location: Location(latitude: 2, longitude: 1), photo: nil),
-                NearbyRestaurant(id: "#3", name: "Place name #3", isOpen: true, rating: 5.0, location: Location(latitude: 4, longitude: 5), photo: nil)
+                NearbyRestaurant(
+                    id: "#1",
+                    name: "Place name #1",
+                    isOpen: true,
+                    rating: 3.4,
+                    location: Location(latitude: 0, longitude: 0),
+                    photo: nil
+                ),
+                NearbyRestaurant(
+                    id: "#2",
+                    name: "Place name #2",
+                    isOpen: false,
+                    rating: 4.3,
+                    location: Location(latitude: 2, longitude: 1),
+                    photo: nil
+                ),
+                NearbyRestaurant(
+                    id: "#3",
+                    name: "Place name #3",
+                    isOpen: true,
+                    rating: 5.0,
+                    location: Location(latitude: 4, longitude: 5),
+                    photo: nil
+                )
             ]
         }
     }
-    
+
     private class PreviewFetchPlacePhotoService: RestaurantPhotoService {
         func fetchPhoto(photoReference: String) async throws -> Data {
             UIImage(named: "restaurant_logo_test")!.pngData()!
