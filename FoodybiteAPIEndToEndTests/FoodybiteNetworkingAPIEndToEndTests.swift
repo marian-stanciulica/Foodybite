8026d2937012dd3edca1bded20101823702dd6be
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:02:32 +0300

    Makes 'FoodybiteNetworkingAPIEndToEndTests' conform to the SwiftLint style conventions

--- FoodybiteAPIEndToEndTests/FoodybiteNetworkingAPIEndToEndTests.swift
+++ FoodybiteAPIEndToEndTests/FoodybiteNetworkingAPIEndToEndTests.swift
@@ -21,11 +21,11 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
                                        profileImage: testingProfileImage)
         })
     }
-    
+
     func test_2endToEndLogin_returnsExpectedUser() async {
         await executeLogin()
     }
-    
+
     func test_3endToEndChangePassword_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().changePassword(currentPassword: testingPassword,
@@ -33,13 +33,13 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
                                                             confirmPassword: testingNewPassword)
         })
     }
-    
+
     func test_4endToEndLogout_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().logout()
         })
     }
-    
+
     func test_5endToEndUpdateAccount_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().updateAccount(name: testingNewName,
@@ -47,7 +47,7 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
                                                            profileImage: testingNewProfileImage)
         })
     }
-    
+
     func test_6endToEndAddReview_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().addReview(
@@ -58,7 +58,7 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
             )
         })
     }
-    
+
     func test_7endToEndGetReviews_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().addReview(
@@ -68,46 +68,46 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
                 createdAt: secondCreatedAt()
             )
         })
-        
+
         await executeGetReviews(restaurantID: firstRestaurantID(), expectedReviews: expectedReviewsForFirstRestaurantID())
         await executeGetReviews(restaurantID: secondRestaurantID(), expectedReviews: expectedReviewsForSecondRestaurantID())
         await executeGetReviews(expectedReviews: expectedAllReviews())
     }
-    
+
     func test_8endToEndDeleteAccount_returnsSuccessfully() async {
         await execute(action: {
             try await makeAuthenticatedSUT().deleteAccount()
         })
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> APIService {
         let session = URLSession(configuration: .ephemeral)
         let httpClient = URLSessionHTTPClient(session: session)
         let tokenStore = KeychainTokenStore()
-        
+
         let remoteResourceLoader = RemoteStore(client: httpClient)
         return APIService(loader: remoteResourceLoader,
                           sender: remoteResourceLoader,
                           tokenStore: tokenStore)
     }
-    
+
     private func makeAuthenticatedSUT() -> APIService {
         let session = URLSession(configuration: .ephemeral)
         let httpClient = URLSessionHTTPClient(session: session)
         let refreshTokenLoader = RemoteStore(client: httpClient)
         let tokenStore = KeychainTokenStore()
-        
+
         let tokenRefresher = RefreshTokenService(loader: refreshTokenLoader, tokenStore: tokenStore)
         let authenticatedHTTPClient = AuthenticatedURLSessionHTTPClient(decoratee: httpClient, tokenRefresher: tokenRefresher)
         let authenticatedRemoteResourceLoader = RemoteStore(client: authenticatedHTTPClient)
-        
+
         return APIService(loader: authenticatedRemoteResourceLoader,
                           sender: authenticatedRemoteResourceLoader,
                           tokenStore: tokenStore)
     }
-    
+
     private func execute(action: () async throws -> Void, file: StaticString = #filePath, line: UInt = #line) async {
         do {
             try await action()
@@ -115,7 +115,7 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successful request, got \(error) instead", file: file, line: line)
         }
     }
-    
+
     private func executeLogin(file: StaticString = #filePath, line: UInt = #line) async {
         do {
             let receivedUser = try await makeSUT().login(email: testingEmail, password: testingPassword)
@@ -124,7 +124,7 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successful login request, got \(error) instead", file: file, line: line)
         }
     }
-    
+
     private func executeGetReviews(restaurantID: String? = nil, expectedReviews: [Review], file: StaticString = #filePath, line: UInt = #line) async {
         do {
             let receivedReviews = try await makeAuthenticatedSUT().getReviews(restaurantID: restaurantID)
@@ -133,78 +133,78 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successful get reviews, got \(error) instead", file: file, line: line)
         }
     }
-    
+
     private var testingName: String {
         "Testing"
     }
-    
+
     private var testingNewName: String {
         "New testing name"
     }
-    
+
     private var testingEmail: String {
         "testing@testing.com"
     }
-    
+
     private var testingPassword: String {
         "123Password321$"
     }
-    
+
     private var testingNewPassword: String {
         "new123Password321$"
     }
-    
+
     private var testingProfileImage: Data? {
         "profile image".data(using: .utf8)
     }
-    
+
     private var testingNewProfileImage: Data? {
         "new profile image".data(using: .utf8)
     }
-    
+
     private var expectedUser: User {
         User(id: UUID(),
              name: testingName,
              email: testingEmail,
              profileImage: testingProfileImage)
     }
-    
+
     private func firstRestaurantID() -> String {
         "first restaurant id"
     }
-    
+
     private func secondRestaurantID() -> String {
         "second restaurant id"
     }
-    
+
     private func firstReviewText() -> String {
         "first review text"
     }
-    
+
     private func secondReviewText() -> String {
         "second review text"
     }
-    
+
     private func firstStarsNumber() -> Int {
         3
     }
-    
+
     private func secondStarsNumber() -> Int {
         5
     }
-    
+
     private func firstCreatedAt() -> Date {
         Date(timeIntervalSince1970: 1675865291)
     }
-    
+
     private func secondCreatedAt() -> Date {
         Date(timeIntervalSince1970: 1562860291)
     }
-    
+
     private func expectedAllReviews() -> [Review] {
         expectedReviewsForFirstRestaurantID() + expectedReviewsForSecondRestaurantID()
     }
-    
+
     private func expectedReviewsForFirstRestaurantID() -> [Review] {
         [
             Review(
@@ -218,7 +218,7 @@ final class FoodybiteNetworkingAPIEndToEndTests: XCTestCase {
             )
         ]
     }
-    
+
     private func expectedReviewsForSecondRestaurantID() -> [Review] {
         [
             Review(
@@ -245,15 +245,15 @@ private extension User {
 private extension Array where Element == Review {
     func isEqual(to reviews: [Review]) -> Bool {
         guard count == reviews.count else { return false }
-        
+
         let equals = (0..<count).filter {
             if !self[$0].isEqual(to: reviews[$0]) {
                 return false
             }
-            
+
             return true
         }
-        
+
         return equals.count == count
     }
 }
@@ -265,3 +265,4 @@ private extension Review {
         rating == review.rating
     }
 }
+
