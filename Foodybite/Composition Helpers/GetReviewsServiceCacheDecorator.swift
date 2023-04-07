7a310a9921e773a166559124a403306c7a40aa67
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:43:23 +0300

    Makes 'GetReviewsServiceCacheDecorator' conform to the SwiftLint style conventions

--- Foodybite/Composition Helpers/GetReviewsServiceCacheDecorator.swift
+++ Foodybite/Composition Helpers/GetReviewsServiceCacheDecorator.swift
@@ -10,12 +10,12 @@ import Domain
 public final class GetReviewsServiceCacheDecorator: GetReviewsService {
     private let getReviewsService: GetReviewsService
     private let cache: ReviewCache
-    
+
     public init(getReviewsService: GetReviewsService, cache: ReviewCache) {
         self.getReviewsService = getReviewsService
         self.cache = cache
     }
-    
+
     public func getReviews(restaurantID: String? = nil) async throws -> [Review] {
         let reviews = try await getReviewsService.getReviews(restaurantID: restaurantID)
         try? await cache.save(reviews: reviews)
