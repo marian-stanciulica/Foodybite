fba39b753c1f65ef83319850f1d5d9c3b9f78dac
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:49:10 +0300

    Makes 'RestaurantReviewCellViewModel' conform to the SwiftLint style conventions

--- FoodybitePresentation/Profile/RestaurantReviewCellViewModel.swift
+++ FoodybitePresentation/Profile/RestaurantReviewCellViewModel.swift
@@ -12,45 +12,45 @@ public class RestaurantReviewCellViewModel: ObservableObject {
     public enum GetRestaurantDetailsError: String, Error {
         case serverError = "An error occured while fetching review details. Please try again later!"
     }
-    
+
     public enum State: Equatable {
         case idle
         case isLoading
         case failure(GetRestaurantDetailsError)
         case success(RestaurantDetails)
     }
-    
+
     private let review: Review
     private let restaurantDetailsService: RestaurantDetailsService
-    
+
     @Published public var getRestaurantDetailsState: State = .idle
-    
+
     public var rating: String {
         "\(review.rating)"
     }
-    
+
     public var restaurantName: String {
         if case let .success(restaurantDetails) = getRestaurantDetailsState {
             return restaurantDetails.name
         }
         return ""
     }
-    
+
     public var restaurantAddress: String {
         if case let .success(restaurantDetails) = getRestaurantDetailsState {
             return restaurantDetails.address
         }
         return ""
     }
-    
+
     public init(review: Review, restaurantDetailsService: RestaurantDetailsService) {
         self.review = review
         self.restaurantDetailsService = restaurantDetailsService
     }
-    
+
     @MainActor public func getRestaurantDetails() async {
         getRestaurantDetailsState = .isLoading
-        
+
         do {
             let restaurantDetails = try await restaurantDetailsService.getRestaurantDetails(restaurantID: review.restaurantID)
             getRestaurantDetailsState = .success(restaurantDetails)
