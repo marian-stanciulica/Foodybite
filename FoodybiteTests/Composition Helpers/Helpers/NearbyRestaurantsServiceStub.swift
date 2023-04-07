c5b02a8639278fbad2f186912d86ad2abe5f19ec
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:05:35 +0300

    Makes 'NearbyRestaurantsServiceStub' conform to the SwiftLint style conventions

--- FoodybiteTests/Composition Helpers/Helpers/NearbyRestaurantsServiceStub.swift
+++ FoodybiteTests/Composition Helpers/Helpers/NearbyRestaurantsServiceStub.swift
@@ -10,14 +10,14 @@ import Domain
 final class NearbyRestaurantsServiceStub: NearbyRestaurantsService {
     private(set) var capturedValues = [(location: Location, radius: Int)]()
     var stub: Result<[NearbyRestaurant], Error>?
-    
+
     func searchNearby(location: Location, radius: Int) async throws -> [NearbyRestaurant] {
         capturedValues.append((location, radius))
-        
+
         if let stub = stub {
             return try stub.get()
         }
-        
+
         return []
     }
 }
