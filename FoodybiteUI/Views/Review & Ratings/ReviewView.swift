436a1d42fde624ee85ea84b4fe040374933666e0
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:51:35 +0300

    Makes 'ReviewView' conform to the SwiftLint style conventions

--- FoodybiteUI/Views/Review & Ratings/ReviewView.swift
+++ FoodybiteUI/Views/Review & Ratings/ReviewView.swift
@@ -12,28 +12,28 @@ import FoodybitePresentation
 public struct ReviewView: View {
     @StateObject var viewModel: ReviewViewModel
     let dismissScreen: () -> Void
-    
+
     public init(viewModel: ReviewViewModel, dismissScreen: @escaping () -> Void) {
         self._viewModel = StateObject(wrappedValue: viewModel)
         self.dismissScreen = dismissScreen
     }
-    
+
     public var body: some View {
         VStack {
             Text("Review & Ratings")
                 .font(.title)
-            
+
             RatingView(stars: $viewModel.starsNumber)
                 .padding()
-            
+
             Text("Rate your experience")
                 .foregroundColor(.gray)
                 .padding()
-            
+
             VStack {
                 TextField("Write your experience", text: $viewModel.reviewText, axis: .vertical)
                     .padding()
-                
+
                 Spacer()
             }
             .frame(height: 150)
@@ -41,33 +41,33 @@ public struct ReviewView: View {
                  RoundedRectangle(cornerRadius: 16)
                      .stroke(Color.gray.opacity(0.2), lineWidth: 2)
             )
-            
+
             Spacer()
-            
+
             MarineButton(title: "Done", isLoading: viewModel.isLoading) {
                 Task {
                     await viewModel.addReview()
                 }
             }
-            
+
             createFeedbackText()
         }
         .padding()
         .arrowBackButtonStyle()
         .onChange(of: viewModel.state) { state in
             guard state == .success else { return }
-            
+
             dismissScreen()
         }
     }
-    
+
     private func createFeedbackText() -> Text {
         if case let .failure(loginError) = viewModel.state {
             return Text(loginError.rawValue)
                 .foregroundColor(.red)
                 .font(.headline)
         }
-        
+
         return Text("")
     }
 }
@@ -78,7 +78,7 @@ struct ReviewView_Previews: PreviewProvider {
             ReviewView(viewModel: ReviewViewModel(restaurantID: "any place id", reviewService: PreviewReviewService()), dismissScreen: {})
         }
     }
-    
+
     private class PreviewReviewService: AddReviewService {
         func addReview(restaurantID: String, reviewText: String, starsNumber: Int, createdAt: Date) async throws {}
     }
