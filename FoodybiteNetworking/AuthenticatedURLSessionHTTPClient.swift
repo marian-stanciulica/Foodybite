dbbc16e8ba97258af81577bb02934c2088cee2dc
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:50:03 +0300

    Makes 'AuthenticatedURLSessionHTTPClient' conform to the SwiftLint style conventions

--- FoodybiteNetworking/AuthenticatedURLSessionHTTPClient.swift
+++ FoodybiteNetworking/AuthenticatedURLSessionHTTPClient.swift
@@ -10,25 +10,25 @@ import Foundation
 public class AuthenticatedURLSessionHTTPClient: HTTPClient {
     private let decoratee: HTTPClient
     private let tokenRefresher: TokenRefresher
-    
+
     public init(decoratee: HTTPClient, tokenRefresher: TokenRefresher) {
         self.decoratee = decoratee
         self.tokenRefresher = tokenRefresher
     }
-    
+
     public func send(_ urlRequest: URLRequest) async throws -> (data: Data, response: HTTPURLResponse) {
         let signedURLRequest = try sign(request: urlRequest)
         let (data, response) = try await decoratee.send(signedURLRequest)
-        
+
         if response.statusCode == 401 {
             try await tokenRefresher.fetchLocallyRemoteToken()
             let signedURLRequest = try sign(request: urlRequest)
             return try await decoratee.send(signedURLRequest)
         }
-        
+
         return (data, response)
     }
-    
+
     private func sign(request: URLRequest) throws -> URLRequest {
         let localAuthToken = try tokenRefresher.getLocalToken()
 
