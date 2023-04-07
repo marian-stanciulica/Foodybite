eb8ee9f5555f997c10903e02442c9d4e6f889820
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 16:03:51 +0300

    Makes 'UserAuthenticatedViewModel' conform to the SwiftLint style conventions

--- Foodybite/UserAuthenticatedViewModel.swift
+++ Foodybite/UserAuthenticatedViewModel.swift
@@ -12,27 +12,27 @@ public final class UserAuthenticatedViewModel: ObservableObject {
     public enum LocationLoadingError: String, Error {
         case unauthorized = "Location couldn't be fetched. Try again!"
     }
-    
+
     public enum State: Equatable {
         case isLoading
         case failure(LocationLoadingError)
         case success(Location)
     }
-    
+
     private let locationProvider: LocationProviding
     @Published public var state: State = .isLoading
-    
+
     public var locationServicesEnabled: Bool {
         locationProvider.locationServicesEnabled
     }
-    
+
     public init(locationProvider: LocationProviding) {
         self.locationProvider = locationProvider
     }
-    
+
     @MainActor public func getCurrentLocation() async {
         state = .isLoading
-        
+
         do {
             let location = try await locationProvider.requestLocation()
             state = .success(location)
