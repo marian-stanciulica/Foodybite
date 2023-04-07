d892a1dc1a8ef7d37a4c6af9f3ced33dc7754436
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:25:32 +0300

    Makes 'RestaurantCellViewModelTests' conform to the SwiftLint style conventions

--- FoodybitePresentationTests/Home/RestaurantCellViewModelTests.swift
+++ FoodybitePresentationTests/Home/RestaurantCellViewModelTests.swift
@@ -10,33 +10,33 @@ import Domain
 import FoodybitePresentation
 
 final class RestaurantCellViewModelTests: XCTestCase {
-    
+
     func test_rating_returnsFormattedRating() {
         let sut = makeSUT()
-        
+
         XCTAssertEqual(sut.rating, rating().formatted)
     }
-    
+
     func test_distanceInKmFromCurrentLocation_computedCorrectly() {
         let sut = makeSUT()
 
         XCTAssertEqual(sut.distance, "353.6")
     }
-    
+
     func test_isOpen_equalsNearbyRestaurantIsOpen() {
         let sut = makeSUT()
 
         XCTAssertEqual(sut.isOpen, isOpen())
     }
-    
+
     func test_restaurantName_equalsNearbyRestaurantName() {
         let sut = makeSUT()
 
         XCTAssertEqual(sut.restaurantName, anyRestaurantName())
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> RestaurantCellViewModel {
         let sut = RestaurantCellViewModel(
             nearbyRestaurant: NearbyRestaurant(
@@ -51,33 +51,33 @@ final class RestaurantCellViewModelTests: XCTestCase {
         )
         return sut
     }
-    
+
     private func rating() -> (raw: Double, formatted: String) {
         (4.52, "4.5")
     }
-    
+
     private func isOpen() -> Bool {
         return true
     }
-    
+
     private func anyRestaurantName() -> String {
         "Restaurant name"
     }
-    
+
     private func anyPhoto() -> Photo {
         Photo(width: 100, height: 200, photoReference: "photo reference")
     }
-    
+
     private var anyError: NSError {
         NSError(domain: "any error", code: 1)
     }
-    
+
     private func anyData() -> Data {
         "any data".data(using: .utf8)!
     }
-    
+
     private var anyLocation: Location {
         Location(latitude: 2.3, longitude: 4.5)
     }
-    
+
 }
