2a36d06b02703a012d54c375e11d022ca14e4ba5
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:51:54 +0300

    Makes 'ReviewViewModel' conform to the SwiftLint style conventions

--- FoodybitePresentation/Review & Ratings/ReviewViewModel.swift
+++ FoodybitePresentation/Review & Ratings/ReviewViewModel.swift
@@ -12,33 +12,33 @@ public final class ReviewViewModel: ObservableObject {
     public enum AddReviewError: String, Error {
         case serverError = "Review couldn't be posted. Try again!"
     }
-    
+
     public enum State: Equatable {
         case idle
         case isLoading
         case failure(AddReviewError)
         case success
     }
-    
+
     private let reviewService: AddReviewService
     private let restaurantID: String
-    
+
     @Published public var state: State = .idle
     @Published public var reviewText = ""
     @Published public var starsNumber = 0
-    
+
     public var isLoading: Bool {
         state == .isLoading
     }
-    
+
     public init(restaurantID: String, reviewService: AddReviewService) {
         self.restaurantID = restaurantID
         self.reviewService = reviewService
     }
-    
+
     @MainActor public func addReview() async {
         state = .isLoading
-        
+
         do {
             try await reviewService.addReview(restaurantID: restaurantID, reviewText: reviewText, starsNumber: starsNumber, createdAt: Date())
             state = .success
