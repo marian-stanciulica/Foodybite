b30e280305cf85b766a035479957aa6b95fb93b7
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:59:19 +0300

    Makes 'DistanceSolverTests' conform to the SwiftLint style conventions

--- FoodybiteLocationTests/DistanceSolverTests.swift
+++ FoodybiteLocationTests/DistanceSolverTests.swift
@@ -10,13 +10,13 @@ import Domain
 import FoodybiteLocation
 
 final class DistanceSolverTests: XCTestCase {
-    
+
     func test_getDistanceInKm_computesDistance() {
         let source = Location(latitude: 4.5, longitude: 2.3)
         let destination = Location(latitude: 4.4, longitude: 2.3)
-        
+
         let result = DistanceSolver.getDistanceInKm(from: source, to: destination)
        XCTAssertEqual(result, 11.1)
     }
-    
+
 }
