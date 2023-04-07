018f8435e49e9ab45c305e231ddc0851e84ca6f9
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:57:05 +0300

    Makes 'ChangePasswordViewSnapshotTests' conform to the SwiftLint style conventions

--- FoodybiteUITests/Snapshot Tests/Profile/Settings/Change Password/ChangePasswordViewSnapshotTests.swift
+++ FoodybiteUITests/Snapshot Tests/Profile/Settings/Change Password/ChangePasswordViewSnapshotTests.swift
@@ -12,41 +12,46 @@ import FoodybitePresentation
 import FoodybiteUI
 
 final class ChangePasswordViewSnapshotTests: XCTestCase {
-    
+
     func test_changePasswordViewIdleState() {
         let sut = makeSUT(state: .idle)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_changePasswordViewIsLoadingState() {
         let sut = makeSUT(currentPassword: "12345678",
                           newPassword: "12345678",
                           confirmPassword: "12345678",
                           state: .isLoading)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_changePasswordViewFailureState() {
         let sut = makeSUT(state: .failure(.passwordError(.passwordDoesntContainSpecialCharacter)))
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     func test_changePasswordViewSuccessState() {
         let sut = makeSUT(state: .success)
-        
+
         assertLightSnapshot(matching: sut, as: .image(on: .iPhone13))
         assertDarkSnapshot(matching: sut, as: .image(on: .iPhone13))
     }
-    
+
     // MARK: - Helpers
-    
-    private func makeSUT(currentPassword: String = "", newPassword: String = "", confirmPassword: String = "", state: ChangePasswordViewModel.Result) -> ChangePasswordView {
+
+    private func makeSUT(
+        currentPassword: String = "",
+        newPassword: String = "",
+        confirmPassword: String = "",
+        state: ChangePasswordViewModel.Result
+    ) -> ChangePasswordView {
         let viewModel = ChangePasswordViewModel(changePasswordService: EmptyChangePasswordService())
         viewModel.result = state
         viewModel.currentPassword = currentPassword
@@ -54,7 +59,7 @@ final class ChangePasswordViewSnapshotTests: XCTestCase {
         viewModel.confirmPassword = confirmPassword
         return ChangePasswordView(viewModel: viewModel)
     }
-    
+
     private class EmptyChangePasswordService: ChangePasswordService {
         func changePassword(currentPassword: String, newPassword: String, confirmPassword: String) async throws {}
     }
