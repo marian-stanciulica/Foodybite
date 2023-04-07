65dc04138ea8643e1f530fdc12eaa4dc8a8842e6
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:56:12 +0300

    Makes 'LocationProviderTests' conform to the SwiftLint style conventions

--- FoodybiteLocationTests/LocationProviderTests.swift
+++ FoodybiteLocationTests/LocationProviderTests.swift
@@ -11,33 +11,33 @@ import Domain
 import FoodybiteLocation
 
 final class LocationProviderTests: XCTestCase {
-    
+
     func test_init_locationManagerDelegateSetToSelf() {
         let (sut, locationManagerSpy) = makeSUT()
-        
+
         XCTAssertIdentical(locationManagerSpy.delegate, sut)
     }
-    
+
     func test_requestWhenInUseAuthorization_callsRequestWhenInUseAuthorizationOnLocationManager() {
         let (sut, locationManagerSpy) = makeSUT()
-        
+
         sut.requestWhenInUseAuthorization()
-        
+
         XCTAssertEqual(locationManagerSpy.requestWhenInUseAuthorizationCallCount, 1)
     }
-    
+
     func test_locationManagerDidChangeAuthorization_setsLocationServicesEnabledAccordingly() {
         assertLocationsServicesEnabled(for: .authorizedWhenInUse, withExpectedResult: true)
         assertLocationsServicesEnabled(for: .authorizedAlways, withExpectedResult: true)
         assertLocationsServicesEnabled(for: .denied, withExpectedResult: false)
         assertLocationsServicesEnabled(for: .restricted, withExpectedResult: false)
     }
-    
+
     func test_requestLocation_callsRequestLocationOnLocationManager() async throws {
         let (sut, locationManagerSpy) = makeSUT()
         sut.locationServicesEnabled = true
         let exp = expectation(description: "Wait for task")
-        
+
         let task = Task {
             exp.fulfill()
             return try await sut.requestLocation()
@@ -45,17 +45,17 @@ final class LocationProviderTests: XCTestCase {
 
         task.cancel()
         await waitForExpectations(timeout: 1.0)
-        
+
         XCTAssertEqual(locationManagerSpy.requestLocationCallCount, 1)
     }
-    
+
     func test_requestLocation_throwsErrorWhenLocationServicesAreDisabled() async throws {
         let (sut, _) = makeSUT()
         sut.locationServicesEnabled = false
-        
+
         await expectRequestLocationError(on: sut)
     }
-    
+
     func test_requestLocation_throwsErrorWhenLocationManagerDidFailWithErrorCalled() async throws {
         let (sut, locationManagerSpy) = makeSUT()
         sut.locationServicesEnabled = true
@@ -63,10 +63,10 @@ final class LocationProviderTests: XCTestCase {
         Task {
             sut.locationManager(manager: locationManagerSpy, didFailWithError: anyError())
         }
-        
+
         await expectRequestLocationError(on: sut)
     }
-    
+
     func test_requestLocation_returnsLocationWhenLocationManagerDidUpdateLocationsCalled() async throws {
         let (sut, locationManagerSpy) = makeSUT()
         sut.locationServicesEnabled = true
@@ -74,30 +74,30 @@ final class LocationProviderTests: XCTestCase {
         Task {
             sut.locationManager(manager: locationManagerSpy, didUpdateLocations: anyLocations().locations)
         }
-        
+
         await expectRequestLocationSuccess(on: sut, expectedLocation: anyLocations().firstLocation)
     }
-    
+
     // MARK: - Helpers
-    
+
     private func makeSUT() -> (sut: LocationProvider, locationManagerSpy: LocationManagerSpy) {
         let locationManagerSpy = LocationManagerSpy()
         let sut = LocationProvider(locationManager: locationManagerSpy)
         return (sut, locationManagerSpy)
     }
-    
+
     private func assertLocationsServicesEnabled(for authorizationStatus: CLAuthorizationStatus,
                                                 withExpectedResult result: Bool,
                                                 file: StaticString = #filePath,
                                                 line: UInt = #line) {
         let (sut, locationManagerSpy) = makeSUT()
         locationManagerSpy.authorizationStatus = authorizationStatus
-        
+
         sut.locationManagerDidChangeAuthorization(manager: locationManagerSpy)
-        
+
         XCTAssertEqual(sut.locationServicesEnabled, result, file: file, line: line)
     }
-    
+
     private func expectRequestLocationError(on sut: LocationProvider,
                                             file: StaticString = #filePath,
                                             line: UInt = #line) async {
@@ -108,7 +108,7 @@ final class LocationProviderTests: XCTestCase {
             XCTAssertNotNil(error, file: file, line: line)
         }
     }
-    
+
     private func expectRequestLocationSuccess(on sut: LocationProvider,
                                               expectedLocation: Location,
                                               file: StaticString = #filePath,
@@ -120,38 +120,38 @@ final class LocationProviderTests: XCTestCase {
             XCTFail("Expected to receive \(anyLocations().firstLocation), got \(error) instead", file: file, line: line)
         }
     }
-    
+
     private func anyError() -> NSError {
         NSError(domain: "any error", code: 1)
     }
-    
+
     private func anyLocations() -> (firstLocation: Location, locations: [CLLocation]) {
         let firstLocation = Location(latitude: 1.1, longitude: 3.2)
-        
+
         let locations = [
             CLLocation(latitude: 1.1, longitude: 3.2),
             CLLocation(latitude: -6.5, longitude: 7.4),
             CLLocation(latitude: 12.4, longitude: -9.2),
             CLLocation(latitude: -112.4, longitude: -54.5)
         ]
-        
+
         return (firstLocation, locations)
     }
-    
+
     private class LocationManagerSpy: LocationManager {
         var delegate: CLLocationManagerDelegate?
         var authorizationStatus: CLAuthorizationStatus = .notDetermined
-        
+
         var requestWhenInUseAuthorizationCallCount = 0
         var requestLocationCallCount = 0
-        
+
         func requestWhenInUseAuthorization() {
             requestWhenInUseAuthorizationCallCount += 1
         }
-        
+
         func requestLocation() {
             requestLocationCallCount += 1
         }
     }
-    
+
 }
