fa51bd71630784286159ae953e053bf5a5062896
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:53:42 +0300

    Makes 'SearchCriteriaView' conform to the SwiftLint style conventions

--- FoodybiteUI/Views/Search Criteria/SearchCriteriaView.swift
+++ FoodybiteUI/Views/Search Criteria/SearchCriteriaView.swift
@@ -12,21 +12,21 @@ import FoodybitePresentation
 public struct SearchCriteriaView: View {
     @StateObject var viewModel: SearchCriteriaViewModel
     @Environment(\.presentationMode) var presentationMode
-    
+
     public init(viewModel: SearchCriteriaViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
     }
-    
+
     public var body: some View {
         VStack {
             HStack {
                 Spacer()
-                
+
                 Text("Filter")
                     .font(.title)
-                
+
                 Spacer()
-                
+
                 Button {
                     presentationMode.wrappedValue.dismiss()
                 } label: {
@@ -37,27 +37,27 @@ public struct SearchCriteriaView: View {
                 }
             }
             .padding(.horizontal)
-            
+
             Text("Distance")
                 .font(.title)
                 .padding(.top)
-            
+
             SelectRadiusView(radius: $viewModel.radius)
                 .padding(.bottom, 56)
-            
+
             Text("Ratings")
                 .font(.title)
                 .padding(.top)
-            
+
             RatingView(stars: $viewModel.starsNumber)
-            
+
             Spacer()
-            
+
             HStack {
                 MarineButton(title: "Reset", isLoading: false) {
                     viewModel.reset()
                 }
-                
+
                 MarineButton(title: "Apply", isLoading: false) {
                     viewModel.apply()
                     presentationMode.wrappedValue.dismiss()
@@ -77,7 +77,7 @@ struct SearchCriteriaView_Previews: PreviewProvider {
             )
         )
     }
-    
+
     private class PreviewUserPreferencesSaver: UserPreferencesSaver {
         func save(_ userPreferences: UserPreferences) {}
     }
