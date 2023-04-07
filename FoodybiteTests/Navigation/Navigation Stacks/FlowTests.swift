d5db71a9f0f4812771759b9744313759ab49ed4d
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 14:42:12 +0300

    Makes 'FlowTests' conform to the SwiftLint style conventions

--- FoodybiteTests/Navigation/Navigation Stacks/FlowTests.swift
+++ FoodybiteTests/Navigation/Navigation Stacks/FlowTests.swift
@@ -9,32 +9,32 @@ import XCTest
 @testable import Foodybite
 
 final class FlowTests: XCTestCase {
-    
+
     func test_authRoute_containsAllCases() {
         XCTAssertEqual(AuthRoute.allCases, [.signUp])
     }
-    
+
     func test_append_appendsValueToNavigationPath() {
         let sut = Flow<AuthRoute>()
-        
+
         XCTAssertEqual(sut.path.count, 0)
-        
+
         sut.append(.signUp)
         XCTAssertEqual(sut.path.count, 1)
     }
-    
+
     func test_navigateBack_removesLastValueFromNavigationPath() {
         let sut = Flow<ProfileRoute>()
-        
+
         sut.append(.settings)
         XCTAssertEqual(sut.path, [.settings])
-        
+
         sut.append(.changePassword)
         XCTAssertEqual(sut.path, [.settings, .changePassword])
-        
+
         sut.navigateBack()
         XCTAssertEqual(sut.path, [.settings])
-        
+
         sut.navigateBack()
         XCTAssertTrue(sut.path.isEmpty)
     }
