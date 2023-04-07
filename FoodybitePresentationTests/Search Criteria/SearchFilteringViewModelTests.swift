129592f06a1d9b6c02b7e5cf679ef11ab43cac81
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:54:46 +0300

    Makes 'SearchCriteriaViewModelTests' conform to the SwiftLint style conventions

--- FoodybitePresentationTests/Search Criteria/SearchFilteringViewModelTests.swift
+++ FoodybitePresentationTests/Search Criteria/SearchFilteringViewModelTests.swift
@@ -15,39 +15,39 @@ final class SearchCriteriaViewModelTests: XCTestCase {
         let radius = 123
         let starsNumber = 3
         let (sut, _) = makeSUT(stub: UserPreferences(radius: radius, starsNumber: starsNumber))
-        
+
         XCTAssertEqual(sut.radius, radius)
         XCTAssertEqual(sut.starsNumber, starsNumber)
     }
-    
+
     func test_apply_savesUserPreferencesUsingUserPreferencesSaver() {
         let expectedUserPreferences = UserPreferences(radius: 123, starsNumber: 3)
         let (sut, userPreferencesSaverSpy) = makeSUT(stub: expectedUserPreferences)
-        
+
         sut.apply()
-        
+
         XCTAssertEqual(userPreferencesSaverSpy.capturedValues, [expectedUserPreferences])
     }
-    
+
     func test_reset_savesDefaultUserPreferences() {
         let (sut, userPreferencesSaverSpy) = makeSUT(stub: UserPreferences(radius: 123, starsNumber: 3))
-        
+
         sut.reset()
-        
+
         XCTAssertEqual(userPreferencesSaverSpy.capturedValues, [.default])
     }
 
     // MARK: - Helpers
-    
+
     private func makeSUT(stub: UserPreferences) -> (sut: SearchCriteriaViewModel, userPreferencesSaverSpy: UserPreferencesSaverSpy) {
         let userPreferencesSaverSpy = UserPreferencesSaverSpy()
         let sut = SearchCriteriaViewModel(userPreferences: stub, userPreferencesSaver: userPreferencesSaverSpy)
         return (sut, userPreferencesSaverSpy)
     }
-    
+
     private class UserPreferencesSaverSpy: UserPreferencesSaver {
         private(set) var capturedValues = [UserPreferences]()
-        
+
         func save(_ userPreferences: UserPreferences) {
             capturedValues.append(userPreferences)
         }
