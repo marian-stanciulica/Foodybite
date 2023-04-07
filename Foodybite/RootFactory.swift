28ddf0c72494498eeb9f4a559a62a2eee67a05bf
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:53:04 +0300

    Makes 'RootFactory' conform to the SwiftLint style conventions

--- Foodybite/RootFactory.swift
+++ Foodybite/RootFactory.swift
@@ -12,29 +12,29 @@ import FoodybitePlaces
 import FoodybiteNetworking
 import FoodybitePersistence
 
-class RootFactory {
+final class RootFactory {
     let apiService: APIService = {
         let session = URLSession(configuration: .ephemeral)
         let httpClient = URLSessionHTTPClient(session: session)
         let tokenStore = KeychainTokenStore()
-        
+
         let remoteResourceLoader = RemoteStore(client: httpClient)
         return APIService(loader: remoteResourceLoader,
                           sender: remoteResourceLoader,
                           tokenStore: tokenStore)
     }()
-        
+
     static let localStore: LocalStore = {
         do {
             return try CoreDataLocalStore(
                 storeURL: NSPersistentContainer
                     .defaultDirectoryURL()
                     .appendingPathComponent("foodybite-store.sqlite"))
-            
+
         } catch {
             return NullLocalStore()
         }
     }()
-    
+
     let userDAO = UserDAO(store: localStore)
 }
