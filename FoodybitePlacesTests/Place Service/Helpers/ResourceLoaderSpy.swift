7ad11996349d12a2776097109a7593712e604a88
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:36:32 +0300

    Makes 'ResourceLoaderSpy' conform to the SwiftLint style conventions

--- FoodybitePlacesTests/Place Service/Helpers/ResourceLoaderSpy.swift
+++ FoodybitePlacesTests/Place Service/Helpers/ResourceLoaderSpy.swift
@@ -8,21 +8,22 @@
 import Foundation
 import FoodybitePlaces
 
-class ResourceLoaderSpy: ResourceLoader, DataLoader {
+final class ResourceLoaderSpy: ResourceLoader, DataLoader {
     private let response: Decodable
     var data: Data?
     var getRequests = [URLRequest]()
     var getDataRequests = [URLRequest]()
-    
+
     init(response: Decodable) {
         self.response = response
     }
-    
-    func get<T>(for urlRequest: URLRequest) async throws -> T where T : Decodable {
+
+    func get<T: Decodable>(for urlRequest: URLRequest) async throws -> T {
         getRequests.append(urlRequest)
+        // swiftlint:disable:next force_cast
         return response as! T
     }
-    
+
     func getData(for urlRequest: URLRequest) async throws -> Data {
         getDataRequests.append(urlRequest)
         return data ?? Data()
