473f2dfe42641c4616d5baddba473526cf113649
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 15:13:09 +0300

    Makes 'PasswordValidatorTests' conform to the SwiftLint style conventions

--- FoodybitePresentationTests/Input Validators/PasswordValidatorTests.swift
+++ FoodybitePresentationTests/Input Validators/PasswordValidatorTests.swift
@@ -13,27 +13,27 @@ final class PasswordValidatorTests: XCTestCase {
     func test_registrationError_rawValueOfEmptyPasswordError() {
         XCTAssertEqual(PasswordValidator.empty.rawValue, "Empty Password")
     }
-    
+
     func test_registrationError_rawValueOfTooShortPasswordError() {
         XCTAssertEqual(PasswordValidator.tooShortPassword.rawValue, "Too short password")
     }
-    
+
     func test_registrationError_rawValueOfPasswordDoesntContainUpperLetterError() {
         XCTAssertEqual(PasswordValidator.passwordDoesntContainUpperLetter.rawValue, "Password should contain at least one uppercase letter")
     }
-    
+
     func test_registrationError_rawValueOfPasswordDoesntContainLowerLetterError() {
         XCTAssertEqual(PasswordValidator.passwordDoesntContainLowerLetter.rawValue, "Password should contain at least one lowercase letter")
     }
-    
+
     func test_registrationError_rawValueOfPasswordDoesntContainDigitsError() {
         XCTAssertEqual(PasswordValidator.passwordDoesntContainDigits.rawValue, "Password should contain at least one digit")
     }
-    
+
     func test_registrationError_rawValueOfPasswordDoesntContainSpecialCharacterError() {
         XCTAssertEqual(PasswordValidator.passwordDoesntContainSpecialCharacter.rawValue, "Password should contain at least one special character")
     }
-    
+
     func test_registrationError_rawValueOfPasswordsDontMatchError() {
         XCTAssertEqual(PasswordValidator.passwordsDontMatch.rawValue, "Passwords do not match")
     }
