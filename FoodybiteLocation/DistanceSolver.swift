f0237f5c21363c7ccf3434949de3a89d438de0ed
Marian25 <marian.stanciulica@gmail.com>
2023-04-07 13:58:59 +0300

    Makes 'DistanceSolver' conform to the SwiftLint style conventions

--- FoodybiteLocation/DistanceSolver.swift
+++ FoodybiteLocation/DistanceSolver.swift
@@ -9,7 +9,7 @@ import Domain
 import CoreLocation
 
 public final class DistanceSolver {
-    
+
     public static func getDistanceInKm(from source: Location, to destination: Location) -> Double {
         let source = CLLocation(latitude: source.latitude, longitude: source.longitude)
         let destination = CLLocation(latitude: destination.latitude, longitude: destination.longitude)
@@ -17,9 +17,9 @@ public final class DistanceSolver {
         let result = source.distance(from: destination) / 1000
         return roundDistance(result)
     }
-    
+
     private static func roundDistance(_ number: Double) -> Double {
         round(number * 10) / 10.0
     }
-    
+
 }
