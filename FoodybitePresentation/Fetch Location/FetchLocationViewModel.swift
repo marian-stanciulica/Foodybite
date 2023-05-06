6cd909b7fc3212c92b8564d573c10f359848c374
Marian25 <marian.stanciulica@gmail.com>
2023-05-06 09:42:52 +0300

    Duplicate UserAuthenticatedViewModel into FetchLocationViewModel and moved to the presentation module

---
+++ FoodybitePresentation/Fetch Location/FetchLocationViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  FetchLocationViewModel.swift
+//  FoodybitePresentation
+//
+//  Created by Marian Stanciulica on 06.05.2023.
+//
+
+import Foundation
+import Domain
+
+public final class FetchLocationViewModel: ObservableObject {
+    public enum LocationLoadingError: String, Error {
+        case unauthorized = "Location couldn't be fetched. Try again!"
+    }
+
+    public enum State: Equatable {
+        case isLoading
+        case failure(LocationLoadingError)
+        case success(Location)
+    }
+
+    private let locationProvider: LocationProviding
+    @Published public var state: State = .isLoading
+
+    public var locationServicesEnabled: Bool {
+        locationProvider.locationServicesEnabled
+    }
+
+    public init(locationProvider: LocationProviding) {
+        self.locationProvider = locationProvider
+    }
+
+    @MainActor public func getCurrentLocation() async {
+        state = .isLoading
+
+        do {
+            let location = try await locationProvider.requestLocation()
+            state = .success(location)
+        } catch {
+            state = .failure(.unauthorized)
+        }
+    }
+}
---
+++ FoodybitePresentationTests/Fetch Location/FetchLocationViewModelTests.swift
@@ -0,0 +1,90 @@
+//
+//  FetchLocationViewModelTests.swift
+//  FoodybitePresentationTests
+//
+//  Created by Marian Stanciulica on 06.05.2023.
+//
+
+import XCTest
+import Domain
+import FoodybitePresentation
+
+final class FetchLocationViewModelTests: XCTestCase {
+
+    func test_locationServicesEnabled_equalsToLocationProviderLocationServicesEnabled() {
+        let (sut, locationProviderSpy) = makeSUT()
+
+        locationProviderSpy.locationServicesEnabled = false
+        XCTAssertFalse(sut.locationServicesEnabled)
+
+        locationProviderSpy.locationServicesEnabled = true
+        XCTAssertTrue(sut.locationServicesEnabled)
+    }
+
+    func test_state_initialStateIsLoading() async {
+        let (sut, _) = makeSUT()
+
+        XCTAssertEqual(sut.state, .isLoading)
+    }
+
+    func test_getCurrentLocation_callsLocationProvider() async {
+        let (sut, locationProviderSpy) = makeSUT()
+
+        await sut.getCurrentLocation()
+
+        XCTAssertEqual(locationProviderSpy.requestLocationCallCount, 1)
+    }
+
+    func test_getCurrentLocation_setsStateToErrorWhenLocationProviderThrowsError() async {
+        let (sut, locationProviderSpy) = makeSUT()
+        locationProviderSpy.result = .failure(anyError())
+
+        await sut.getCurrentLocation()
+
+        XCTAssertEqual(sut.state, .failure(.unauthorized))
+    }
+
+    func test_getCurrentLocation_setsStateToLoadedWhenLocationProviderReturnsLocation() async {
+        let (sut, locationProviderSpy) = makeSUT()
+        locationProviderSpy.result = .success(anyLocation())
+
+        await sut.getCurrentLocation()
+
+        XCTAssertEqual(sut.state, .success(anyLocation()))
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> (sut: FetchLocationViewModel, locationProviderSpy: LocationProvidingSpy) {
+        let locationProviderSpy = LocationProvidingSpy()
+        let sut = FetchLocationViewModel(locationProvider: locationProviderSpy)
+        return (sut, locationProviderSpy)
+    }
+
+    private func anyError() -> NSError {
+        NSError(domain: "any error", code: 1)
+    }
+
+    private func anyLocation() -> Location {
+        Location(latitude: 2.3, longitude: 4.5)
+    }
+
+    private class LocationProvidingSpy: LocationProviding {
+        var locationServicesEnabled: Bool = false
+        var result: Result<Location, Error>?
+        private(set) var requestLocationCallCount = 0
+
+        func requestLocation() async throws -> Location {
+            requestLocationCallCount += 1
+
+            if let result = result {
+                return try result.get()
+            }
+
+            throw NSError(domain: "any error", code: 1)
+        }
+
+        func requestWhenInUseAuthorization() {}
+    }
+
+}
