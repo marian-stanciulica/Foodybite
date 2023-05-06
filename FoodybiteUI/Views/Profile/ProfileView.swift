5078474086ba4ed04b8784be59d1ff36738ecf91
Marian25 <marian.stanciulica@gmail.com>
2023-05-06 11:09:25 +0300

    ProfileView - extracts view builders methods from body to improve readability

--- FoodybiteUI/Views/Profile/ProfileView.swift
+++ FoodybiteUI/Views/Profile/ProfileView.swift
@@ -33,92 +33,10 @@ public struct ProfileView<Cell: View>: View {
     public var body: some View {
         ScrollView {
             VStack {
-                if let imageData = viewModel.user.profileImage, let uiImage = UIImage(data: imageData) {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .frame(width: 150, height: 150)
-                        .cornerRadius(100)
-                } else {
-                    InitialsView(initials: String(viewModel.user.name.prefix(1)))
-                        .frame(width: 150, height: 150)
-                }
-
-                Text(viewModel.user.name)
-                    .font(.title)
-
-                Text(viewModel.user.email)
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-
-                HStack {
-                    switch viewModel.getReviewsState {
-                    case .isLoading:
-                        ProgressView()
-                            .tint(.primary)
-                    case .idle, .failure:
-                        StatsView(stats: "0", description: "Reviews")
-                    case let .success(reviews):
-                        StatsView(stats: "\(reviews.count)", description: "Reviews")
-                    }
-
-                    Spacer()
-                    Divider()
-                    Spacer()
-
-                    StatsView(stats: "0", description: "Followers")
-
-                    Spacer()
-                    Divider()
-                    Spacer()
-
-                    StatsView(stats: "0", description: "Following")
-                }
-                .padding(.horizontal, 56)
-                .padding(.vertical)
-                .task {
-                    guard viewModel.getReviewsState == .idle else { return }
-
-                    await viewModel.getAllReviews()
-                }
-
-                HStack {
-                    MarineButton(title: "Edit Profile", isLoading: false) {
-                        editProfileAlertDisplayed = true
-                    }
-                    .padding(.horizontal)
-
-                    WhiteButton(title: "Settings") {
-                        goToSettings()
-                    }
-                    .padding(.horizontal)
-                }
-                .padding(.horizontal, 32)
-
-                Rectangle()
-                    .frame(maxWidth: .infinity, maxHeight: 2)
-                    .foregroundColor(.gray.opacity(0.2))
-                    .padding(.top)
-
-                switch viewModel.getReviewsState {
-                case .idle:
-                    EmptyView()
-                case .isLoading:
-                    ProgressView()
-                        .tint(.primary)
-                case let .failure(error):
-                    Text(error.rawValue)
-                        .padding()
-                        .foregroundColor(.red)
-                        .font(.headline)
-                case let .success(reviews):
-                    LazyVStack {
-                        ForEach(reviews, id: \.id) { review in
-                            cell(review)
-                                .padding(.horizontal)
-                                .padding(.vertical, 4)
-                        }
-                    }
-                }
+                makeUserIdentificationView()
+                makeUserStatistics()
+                makeNavigationButtons()
+                makeReviews()
             }
             .navigationTitle("My Profile")
             .navigationBarTitleDisplayMode(.inline)
@@ -130,6 +48,101 @@ public struct ProfileView<Cell: View>: View {
                     }
                 }
                 Button("Cancel", role: .cancel) {}
+            }
+        }
+    }
+
+    @ViewBuilder private func makeUserIdentificationView() -> some View {
+        if let imageData = viewModel.user.profileImage, let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .frame(width: 150, height: 150)
+                .cornerRadius(100)
+        } else {
+            InitialsView(initials: String(viewModel.user.name.prefix(1)))
+                .frame(width: 150, height: 150)
+        }
+
+        Text(viewModel.user.name)
+            .font(.title)
+
+        Text(viewModel.user.email)
+            .font(.footnote)
+            .foregroundColor(.gray)
+    }
+
+    @ViewBuilder private func makeUserStatistics() -> some View {
+        HStack {
+            switch viewModel.getReviewsState {
+            case .isLoading:
+                ProgressView()
+                    .tint(.primary)
+            case .idle, .failure:
+                StatsView(stats: "0", description: "Reviews")
+            case let .success(reviews):
+                StatsView(stats: "\(reviews.count)", description: "Reviews")
+            }
+
+            Spacer()
+            Divider()
+            Spacer()
+
+            StatsView(stats: "0", description: "Followers")
+
+            Spacer()
+            Divider()
+            Spacer()
+
+            StatsView(stats: "0", description: "Following")
+        }
+        .padding(.horizontal, 56)
+        .padding(.vertical)
+        .task {
+            guard viewModel.getReviewsState == .idle else { return }
+
+            await viewModel.getAllReviews()
+        }
+    }
+
+    @ViewBuilder private func makeNavigationButtons() -> some View {
+        HStack {
+            MarineButton(title: "Edit Profile", isLoading: false) {
+                editProfileAlertDisplayed = true
+            }
+            .padding(.horizontal)
+
+            WhiteButton(title: "Settings") {
+                goToSettings()
+            }
+            .padding(.horizontal)
+        }
+        .padding(.horizontal, 32)
+    }
+
+    @ViewBuilder private func makeReviews() -> some View {
+        Rectangle()
+            .frame(maxWidth: .infinity, maxHeight: 2)
+            .foregroundColor(.gray.opacity(0.2))
+            .padding(.top)
+
+        switch viewModel.getReviewsState {
+        case .idle:
+            EmptyView()
+        case .isLoading:
+            ProgressView()
+                .tint(.primary)
+        case let .failure(error):
+            Text(error.rawValue)
+                .padding()
+                .foregroundColor(.red)
+                .font(.headline)
+        case let .success(reviews):
+            LazyVStack {
+                ForEach(reviews, id: \.id) { review in
+                    cell(review)
+                        .padding(.horizontal)
+                        .padding(.vertical, 4)
+                }
             }
         }
     }
